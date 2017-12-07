f266a4692ab723fbc921556d833de10df4c1d354
Jaco Greeff <jacogr@gmail.com>
2017-12-07 20:08:48 +0100

    Add hexFixLength documentation

--- docs/hex.md
+++ docs/hex.md
@@ -3,6 +3,7 @@
 Internal utilities to create and test for hex values 
 
 - [hexAddPrefix](#hexaddprefix) Adds the `0x` prefix to string values.
+- [hexFixLength](#hexfixlength) Pads or shifts a hex string to a specific bitLength
 - [hexFromBn](#hexfrombn) Creates a hex value from a BN.js bignumber object.
 - [hexFromBn](#hexfrombn) Creates a hex value from a Buffer object.
 - [hexFromNumber](#hexfromnumber) Creates a hex value from a number.
@@ -27,6 +28,24 @@ Returns a `0x` prefixed string from the input value. If the input is already pre
 import { hexAddPrefix } from '@polkadot/util';
 
 console.log('With prefix', hexAddPrefix('0a0b12')) // => 0x0a0b12
+```
+
+## hexFixLength
+
+Pads or shifts a hex string to a specific bitLength 
+
+```js
+hexFixLength (value: string, bitLength: number = -1): string
+```
+
+
+Returns a `0x` prefixed string with the specified number of bits contained in the return value. (If bitLength is -1, length checking is not done). Input values with less bits are zero-padded, values with more bits are trimmed to the specified length.
+
+```js
+import { hexFixLength } from '@polkadot/util';
+
+console.log('fixed', hexFixLength('0x12', 16)) // => 0x0012
+console.log('fixed', hexFixLength('0x0012', 8)) // => 0x12
 ```
 
 ## hexFromBn
--- src/hex/fixLength.js
+++ src/hex/fixLength.js
@@ -5,9 +5,20 @@ const assert = require('../assert');
 const isHex = require('../is/hex');
 const hexAddPrefix = require('./addPrefix');
 const hexStripPrefix = require('./stripPrefix');
+const { ZEROS_256 } = require('./constants');
 
-const ZEROS = '0000000000000000000000000000000000000000000000000000000000000000';
+/**
+  @name hexFixLength
+  @signature hexFixLength (value: string, bitLength: number = -1): string
+  @summary Pads or shifts a hex string to a specific bitLength
+  @description
+    Returns a `0x` prefixed string with the specified number of bits contained in the return value. (If bitLength is -1, length checking is not done). Input values with less bits are zero-padded, values with more bits are trimmed to the specified length.
+  @example
+    import { hexFixLength } from '@polkadot/util';
 
+    console.log('fixed', hexFixLength('0x12', 16)) // => 0x0012
+    console.log('fixed', hexFixLength('0x0012', 8)) // => 0x12
+*/
 module.exports = function hexFixLength (value: string, bitLength: number = -1): string {
   assert(isHex(value), `Expected hex input value, found '${value}' instead`);
 
@@ -15,9 +26,20 @@ module.exports = function hexFixLength (value: string, bitLength: number = -1): 
     return value;
   }
 
-  const byteLength = -1 * (bitLength / 8) * 2;
+  const strLength = bitLength / 4;
+  const hexLength = strLength + 2;
+
+  if (value.length === hexLength) {
+    return value;
+  }
+
+  if (value.length > hexLength) {
+    return hexAddPrefix(
+      hexStripPrefix(value).slice(-1 * strLength)
+    );
+  }
 
   return hexAddPrefix(
-    `${ZEROS}${hexStripPrefix(value)}`.slice(byteLength)
+    `${ZEROS_256}${hexStripPrefix(value)}`.slice(-1 * strLength)
   );
 };
