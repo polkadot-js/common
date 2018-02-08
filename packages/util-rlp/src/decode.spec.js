187ca097f7c20ac6bdee7e54c265b7d6f4163033
Jaco Greeff <jacogr@gmail.com>
2018-02-08 14:35:30 +0100

    Retrieve tests from ethereumjs-testing

--- packages/util-rlp/src/decode.spec.js
+++ packages/util-rlp/src/decode.spec.js
@@ -2,15 +2,11 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-// const BN = require('bn.js');
-
-// const bnToU8a = require('@polkadot/util/bn/toU8a');
 const hexToU8a = require('@polkadot/util/hex/toU8a');
 const u8aToHex = require('@polkadot/util/u8a/toHex');
 
+const rlptests = require('../test/rlptests');
 const { decode, encode } = require('./index');
-
-const officalTests = require('../test/rlptest.json');
 
 describe('decode', () => {
   it('returns empty list for undefined inputs', () => {
@@ -29,20 +25,17 @@ describe('decode', () => {
     );
   });
 
-  Object
-    .keys(officalTests)
-    .map((name) => ({ name, test: officalTests[name] }))
-    .forEach(({ name, test }) => {
-      it(`passes official ${name}`, () => {
-        expect(
-          u8aToHex(
-            encode(
-              decode(
-                hexToU8a(`0x${test.out}`)
-              )
+  rlptests.forEach(({ name, output }) => {
+    it(`passes official ${name}`, () => {
+      expect(
+        u8aToHex(
+          encode(
+            decode(
+              hexToU8a(output)
             )
           )
-        ).toEqual(`0x${test.out}`);
-      });
+        )
+      ).toEqual(output);
     });
+  });
 });
--- packages/util-rlp/src/encode.spec.js
+++ packages/util-rlp/src/encode.spec.js
@@ -2,30 +2,19 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-const BN = require('bn.js');
-
-const bnToU8a = require('@polkadot/util/bn/toU8a');
 const u8aToHex = require('@polkadot/util/u8a/toHex');
 
 const { encode } = require('./index');
-
-const officalTests = require('../test/rlptest.json');
+const rlptests = require('../test/rlptests');
 
 describe('encode', () => {
-  Object
-    .keys(officalTests)
-    .map((name) => ({ name, test: officalTests[name] }))
-    .forEach(({ name, test }) => {
-      it(`passes official ${name}`, () => {
-        expect(
-          u8aToHex(
-            encode(
-              test.in[0] !== '#'
-                ? test.in
-                : bnToU8a(new BN(test.in.slice(1)))
-            )
-          )
-        ).toEqual(`0x${test.out}`);
-      });
+  rlptests.forEach(({ name, input, output }) => {
+    it(`passes official ${name}`, () => {
+      expect(
+        u8aToHex(
+          encode(input)
+        )
+      ).toEqual(output);
     });
+  });
 });
