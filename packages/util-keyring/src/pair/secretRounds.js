f74ad62e449d8aded262d75f36e911a28b7a8546
Jaco Greeff <jacogr@gmail.com>
2018-03-13 15:47:06 +0100

    Pass salt as blake2b input

--- packages/util-keyring/src/pair/secretRounds.js
+++ packages/util-keyring/src/pair/secretRounds.js
@@ -4,14 +4,13 @@
 // @flow
 
 const blake2AsU8a256 = require('@polkadot/util-crypto/blake2/asU8a256');
-const u8aConcat = require('@polkadot/util/u8a/concat');
 const u8aFromUtf8 = require('@polkadot/util/u8a/fromUtf8');
 
 module.exports = function secretRounds (_secret: string, rounds: number, salt: Uint8Array): Uint8Array {
-  let secret = u8aConcat(u8aFromUtf8(_secret), salt);
+  let secret = u8aFromUtf8(_secret);
 
   for (let count = 0; count < rounds; count++) {
-    secret = blake2AsU8a256(secret);
+    secret = blake2AsU8a256(secret, salt);
   }
 
   return secret;
