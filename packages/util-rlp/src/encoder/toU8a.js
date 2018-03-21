3c3f973f557c2714cf1be7cf58452c3c9c039048
Jaco Greeff <jacogr@gmail.com>
2018-03-21 16:37:43 +0100

    Address u8a conversion complexity

--- packages/util-rlp/src/encoder/toU8a.js
+++ packages/util-rlp/src/encoder/toU8a.js
@@ -3,34 +3,46 @@
 // of the ISC license. See the LICENSE file for details.
 // @flow
 
+const assert = require('@polkadot/util/assert');
 const bnToU8a = require('@polkadot/util/bn/toU8a');
 const bufferToU8a = require('@polkadot/util/buffer/toU8a');
 const hexHasPrefix = require('@polkadot/util/hex/hasPrefix');
 const hexToU8a = require('@polkadot/util/hex/toU8a');
-const isInstanceOf = require('@polkadot/util/is/instanceOf');
 const isBn = require('@polkadot/util/is/bn');
 const isBuffer = require('@polkadot/util/is/buffer');
 const isNull = require('@polkadot/util/is/null');
 const isNumber = require('@polkadot/util/is/number');
 const isString = require('@polkadot/util/is/string');
+const isU8a = require('@polkadot/util/is/u8a');
 const isUndefined = require('@polkadot/util/is/undefined');
 const numberToU8a = require('@polkadot/util/number/toU8a');
 const u8aFromUtf8 = require('@polkadot/util/u8a/fromUtf8');
 
 // flowlint-next-line unclear-type:off
-module.exports = function toU8a (value: any): Uint8Array {
+function fromEmpty (value: any): ?Uint8Array {
   if (isNull(value) || isUndefined(value)) {
     return new Uint8Array([]);
   }
+}
 
+// flowlint-next-line unclear-type:off
+function fromObject (value: any): ?Uint8Array {
   if (isBuffer(value)) {
     return bufferToU8a(value);
   }
 
-  if (isInstanceOf(value, Uint8Array)) {
+  // NOTE: U8a after Buffer check (internally compatible)
+  if (isU8a(value)) {
     return value;
   }
 
+  if (isBn(value)) {
+    return bnToU8a(value);
+  }
+}
+
+// flowlint-next-line unclear-type:off
+function fromPrimitive (value: any): ?Uint8Array {
   if (isNumber(value)) {
     return numberToU8a(value);
   }
@@ -42,10 +54,14 @@ module.exports = function toU8a (value: any): Uint8Array {
 
     return u8aFromUtf8(value);
   }
+}
 
-  if (isBn(value)) {
-    return bnToU8a(value);
-  }
+// flowlint-next-line unclear-type:off
+module.exports = function toU8a (value: any): Uint8Array {
+  const result = fromEmpty(value) || fromObject(value) || fromPrimitive(value);
 
-  throw new Error('invalid type');
+  assert(result, 'invalid type');
+
+  // flowlint-next-line unclear-type:off
+  return ((result: any): Uint8Array);
 };
