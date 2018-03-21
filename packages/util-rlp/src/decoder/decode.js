168ba766a1180444463e933346818da44d6bc1c9
Jaco Greeff <jacogr@gmail.com>
2018-03-21 16:57:08 +0100

    Cleanup decode value checks

--- packages/util-rlp/src/decoder/decode.js
+++ packages/util-rlp/src/decoder/decode.js
@@ -11,42 +11,19 @@ const decodeNumber = require('./number');
 const decodeSingle = require('./single');
 const decodeString = require('./string');
 
-function fromSingle (input: Uint8Array): ?DecodeOutput {
-  // a single byte whose value is in the [0x00, 0x7f] range, that byte is its own RLP encoding
-  if (input[0] <= 0x7f) {
-    return decodeSingle(decode, input);
-  }
-}
-
-function fromString (input: Uint8Array): ?DecodeOutput {
-  // string is 0-55 bytes long. A single byte with value 0x80 plus the length of the string followed by the string
-  // The range of the first byte is [0x80, 0xb7]
-  if (input[0] <= 0xb7) {
-    return decodeString(decode, input);
-  }
-}
-
-function fromNumber (input: Uint8Array): ?DecodeOutput {
-  // a number
-  if (input[0] <= 0xbf) {
-    return decodeNumber(decode, input);
-  }
-}
-
-function fromShortList (input: Uint8Array): ?DecodeOutput {
-  // a list between  0-55 bytes long
-  if (input[0] <= 0xf7) {
-    return decodeListShort(decode, input);
-  }
-}
+const decoders = [
+  { max: 0x7f, fn: decodeSingle },
+  { max: 0xb7, fn: decodeString },
+  { max: 0xbf, fn: decodeNumber },
+  { max: 0xf7, fn: decodeListShort },
+  { max: 0xff, fn: decodeListLong }
+];
 
 function decode (input: Uint8Array): DecodeOutput {
-  // NOTE: Order here is important
-  return fromSingle(input) ||
-    fromString(input) ||
-    fromNumber(input) ||
-    fromShortList(input) ||
-    decodeListLong(decode, input);
+  return decoders
+    .find(({ max }) => input[0] <= max)
+    // $FlowFixMe last entry (listLong) is max uint8 value
+    .fn(decode, input);
 }
 
 module.exports = decode;
