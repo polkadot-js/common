68e77f3abbc553b3aa4ade39022640afb8e47061
Jaco Greeff <jacogr@gmail.com>
2018-04-23 09:34:31 +0200

    toHexShort utility (#119)

--- packages/util/src/u8a/index.js
+++ packages/util/src/u8a/index.js
@@ -13,6 +13,7 @@ const u8aFromString = require('./fromString');
 const u8aToBn = require('./toBn');
 const u8aToBuffer = require('./toBuffer');
 const u8aToHex = require('./toHex');
+const u8aToHexShort = require('./toHexShort');
 const u8aToString = require('./toString');
 const u8aToU8a = require('./toU8a');
 const u8aToUtf8 = require('./toUtf8');
@@ -31,6 +32,7 @@ module.exports = {
   u8aToBn,
   u8aToBuffer,
   u8aToHex,
+  u8aToHexShort,
   u8aToString,
   u8aToU8a,
   u8aToUtf8
