2d86d0602b9c2565e2d29f4c9b94062ae5778c9f
Jaco Greeff <jacogr@gmail.com>
2018-05-24 14:44:33 +0200

    Basic cleanups

--- packages/util/src/u8a/toHex.js
+++ packages/util/src/u8a/toHex.js
@@ -15,21 +15,23 @@
     u8aToHex(new Uint8Array([0x68, 0x65, 0x6c, 0x6c, 0xf])); // 0x68656c0f
 */
 module.exports = function u8aToHex (value?: Uint8Array, bitLength: number = -1, isPrefixed: boolean = true): string {
+  const prefix = isPrefixed
+    ? '0x'
+    : '';
+
   if (!value || !value.length) {
-    return isPrefixed
-      ? '0x'
-      : '';
+    return prefix;
   }
 
   const byteLength = Math.ceil(bitLength / 8);
 
-  if (byteLength <= 0 || value.length <= byteLength) {
-    return value.reduce((result, item) => {
-      return result + `0${item.toString(16)}`.slice(-2);
-    }, isPrefixed ? '0x' : '');
+  if (byteLength > 0 && value.length > byteLength) {
+    const halfLength = Math.ceil(byteLength / 2);
+
+    return `${u8aToHex(value.subarray(0, halfLength), -1, isPrefixed)}…${u8aToHex(value.subarray(value.length - halfLength), -1, false)}`;
   }
 
-  const halfLength = Math.ceil(byteLength / 2);
-
-  return `${u8aToHex(value.subarray(0, halfLength), -1, isPrefixed)}…${u8aToHex(value.subarray(value.length - halfLength), -1, false)}`;
+  return value.reduce((result, item) => {
+    return result + `0${item.toString(16)}`.slice(-2);
+  }, prefix);
 };
