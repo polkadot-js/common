e6aa792cac55cd42181fdc5ac41d7452fab8a79e
Jaco Greeff <jacogr@gmail.com>
2018-06-21 16:21:26 +0200

    Update headers

--- packages/util-rlp/src/encode.spec.js
+++ packages/util-rlp/src/encode.spec.js
@@ -1,6 +1,6 @@
 // Copyright 2017-2018 @polkadot/util-rlp authors & contributors
 // This software may be modified and distributed under the terms
-// of the ISC license. See the LICENSE file for details.
+// of the MPL-2.0 license. See the LICENSE file for details.
 
 import u8aToHex from '@polkadot/util/u8a/toHex';
 
