2f1aebe9081ffa62b38d374798d805f575fa9c63
Jaco Greeff <jacogr@gmail.com>
2018-06-26 16:49:17 +0200

    Update @polkadot/dev, finsing deasync jest resolution

--- packages/util/src/syncify.ts
+++ packages/util/src/syncify.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-const deasync = require('deasync');
+import deasync from 'deasync';
 
 import isError from './is/error';
 import isUndefined from './is/undefined';
