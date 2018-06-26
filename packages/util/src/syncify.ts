69239d56e62e90120dc6417eda27f0b44f16ced3
Jaco Greeff <jacogr@gmail.com>
2018-06-26 16:38:24 +0200

    Use Jest default files as fallback

--- packages/util/src/syncify.ts
+++ packages/util/src/syncify.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import deasync from 'deasync';
+const deasync = require('deasync');
 
 import isError from './is/error';
 import isUndefined from './is/undefined';
