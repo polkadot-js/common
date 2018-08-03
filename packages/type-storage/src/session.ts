11140408e843f5a486c9613ab8e3af6156c90467
Jaco Greeff <jacogr@gmail.com>
2018-08-03 11:00:51 +0200

    Update session with new keys

--- packages/type-storage/src/session.ts
+++ packages/type-storage/src/session.ts
@@ -8,6 +8,20 @@ import { Storages, Storage$Sections } from './types';
 import param from '@polkadot/params/param';
 import createSection from '@polkadot/params/section';
 
+const validators: CreateItemOptions = {
+  description: 'Current validators',
+  key: 'ses:val',
+  params: [],
+  type: ['AccountId']
+};
+
+const length: CreateItemOptions = {
+  description: 'Current length of the session',
+  key: 'ses:len',
+  params: [],
+  type: 'BlockNumber'
+};
+
 const currentIndex: CreateItemOptions = {
   description: 'Current index of the session',
   key: 'ses:ind',
@@ -15,7 +29,28 @@ const currentIndex: CreateItemOptions = {
   type: 'BlockNumber'
 };
 
-const lastSessionChange: CreateItemOptions = {
+const currentStart: CreateItemOptions = {
+  description: 'Timestamp when current session started',
+  key: 'ses:current_start',
+  params: [],
+  type: 'Timestamp'
+};
+
+const brokenPercentLate: CreateItemOptions = {
+  description: 'Percent by which the session must finish late before early-exit',
+  key: 'ses:broken_percent_late',
+  params: [],
+  type: 'u64'
+};
+
+const forcingNewSession: CreateItemOptions = {
+  description: 'New session is being forced is this entry exists',
+  key: 'ses:forcing_new_session',
+  params: [],
+  type: 'bool'
+};
+
+const lastLengthChange: CreateItemOptions = {
   description: 'Block at which the session length last changed',
   key: 'ses:llc',
   params: [],
@@ -38,28 +73,20 @@ const nextLength: CreateItemOptions = {
   type: 'BlockNumber'
 };
 
-const length: CreateItemOptions = {
-  description: 'Current length of the session',
-  key: 'ses:len',
-  params: [],
-  type: 'BlockNumber'
-};
-
-const validators: CreateItemOptions = {
-  description: 'Current validators',
-  key: 'ses:val',
-  params: [],
-  type: ['AccountId']
-};
-
 export default (name: Storage$Sections): Section<Storages> =>
   createSection(name)((createMethod: CreateItems<Storages>) => ({
     description: 'Session management information',
     public: {
+      brokenPercentLate:
+        createMethod('brokenPercentLate')(brokenPercentLate),
       currentIndex:
         createMethod('currentIndex')(currentIndex),
-      lastSessionChange:
-        createMethod('lastSessionChange')(lastSessionChange),
+      currentStart:
+        createMethod('currentStart')(currentStart),
+      forcingNewSession:
+        createMethod('forcingNewSession')(forcingNewSession),
+      lastLengthChange:
+        createMethod('lastLengthChange')(lastLengthChange),
       nextKeyFor:
         createMethod('nextKeyFor')(nextKeyFor),
       nextLength:
