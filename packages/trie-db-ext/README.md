fa6591e27eb2897e8d1bddb7ac4e7f2590d61a7b
Jaco Greeff <jacogr@gmail.com>
2018-09-01 21:37:11 +0200

    trie-d -> trie-db-old, trie-db-ext -> trie-db

--- packages/trie-db-ext/README.md
+++
@@ -1,3 +0,0 @@
-# @polkadot/trie-db-ext
-
-A re-implementation of a Patricia Trie. Unlike other implementations, this allows for the specification of a hash function as well as encoder/decoder and operates in a sync fashion by default.
---
+++ packages/trie-db-old/src/index.ts
@@ -0,0 +1,23 @@
+// Copyright 2015-2018 authors & contributors of https://github.com/ethereumjs/merkle-patricia-tree
+// Copyright 2017-2018 @polkadot/trie-db authors & contributors
+// This software may be modified and distributed under the terms
+// of the MPL-2.0 license. See the LICENSE file for details.
+
+import { HashFn } from './types';
+
+// import logger from '@polkadot/util/logger';
+import CheckpointTrie from './CheckpointTrie';
+import { prove, verifyProof } from './proof';
+
+// const l = logger('trie');
+
+export default class Trie extends CheckpointTrie {
+  static prove = prove;
+  static verifyProof = verifyProof;
+
+  constructor (db: any, root?: Uint8Array, hashing?: HashFn) {
+    super(db, root, hashing);
+
+    // l.debug('Created Trie');
+  }
+}
