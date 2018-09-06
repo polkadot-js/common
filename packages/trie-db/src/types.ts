56f80aa79b99baca1446c5d434f4ffb528bd8b92
Jaco Greeff <jacogr@gmail.com>
2018-09-06 20:58:17 +0200

    Update (linting)

--- packages/trie-db/src/types.ts
+++ packages/trie-db/src/types.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
-import { TxDb, ProgressCb, ProgressValue } from '@polkadot/db/types';
+import { TxDb, ProgressCb } from '@polkadot/db/types';
 
 export enum NodeType {
   EMPTY = 0,
