e987c2199a43a92b61a90c0372fa9bf7ce4617b5
Amaury Martiny <amaury.martiny@protonmail.com>
2018-09-27 14:07:39 +0100

    Add stringLowerFirst

--- packages/util/src/string/index.ts
+++ packages/util/src/string/index.ts
@@ -2,11 +2,13 @@
 // This software may be modified and distributed under the terms
 // of the ISC license. See the LICENSE file for details.
 
+import stringLowerFirst from './lowerFirst';
 import stringShorten from './shorten';
 
 /**
  * @summary Utility methods to convert to work with `string` values
  */
 export {
+  stringLowerFirst,
   stringShorten
 };
---
+++ packages/util/src/string/lowerFirst.ts
@@ -0,0 +1,25 @@
+// Copyright 2017-2018 @polkadot/util authors & contributors
+// This software may be modified and distributed under the terms
+// of the ISC license. See the LICENSE file for details.
+
+import isNull from '../is/null';
+import isUndefined from '../is/undefined';
+
+/**
+ * @name stringLowerFirst
+ * @signature stringLowerFirst (_value: string): string
+ * @summary Lowercase the first letter of a string
+ * @description
+ * Lowercase the first letter of a string
+ * @example
+ *   import { stringLowerFirst } from '@polkadot/util';
+ *
+ *   stringLowerFirst('ABC'); // => 'aBC'
+ */
+export default function stringLowerFirst (_value: string): string {
+  if (isNull(_value) || isUndefined(_value)) {
+    return _value;
+  }
+
+  return _value.charAt(0).toLowerCase() + _value.slice(1);
+}
