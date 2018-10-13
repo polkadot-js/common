016b8b18092443c5c0df9981c9ae9cbcc390bfb8
Jaco Greeff <jacogr@gmail.com>
2018-10-13 17:12:58 +0200

    Warn on unexpected decoded prefix (#223)

--- packages/keyring/src/address/defaults.ts
+++ packages/keyring/src/address/defaults.ts
@@ -8,5 +8,6 @@ export default {
   allowedDecodedLengths: [1, 2, 4, 8, 32],
   // publicKey has prefix + 2 checksum bytes, short only prefix + 1 checksum byte
   allowedEncodedLengths: [3, 4, 6, 10, 35],
+  allowedPrefix: [0, 1, 42, 43, 68, 69] as Array<Prefix>,
   prefix: 42 as Prefix
 };
