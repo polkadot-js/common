a483d8d4191bdbaf36c080612e6626e22681a1b1
Luke Schoen <ltfschoen@gmail.com>
2018-10-17 22:59:50 +0200

    docs: Add Typedocs with examples for db MemoryDb and DiskDb

--- packages/db/src/Disk.ts
+++ packages/db/src/Disk.ts
@@ -8,6 +8,61 @@ import FileFlatDb from './FileFlatDb';
 import LruDb from './engines/LruDb';
 import TransactionDb from './engines/TransactionDb';
 
+/**
+ * @name DiskDb
+ * @summary Creates DiskDb database using LruDb for caching with FileFlatDb and extending TransactionDb.
+ * @description Create an instance of a Disk database using
+ * LruDb (for caching with FileFlatDb) and extending TransactionDb.
+ * Note that LruDB uses the [Least Recently Used (LRU) cache algorithm](https://www.npmjs.com/package/lru_map)
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * import u8aFromString from '@polkadot/util/u8a/fromString';
+ * import DiskDb from '@polkadot/db/Disk';
+ * import TransactionDb from '@polkadot/db/engines/TransactionDb';
+ *
+ * // Creat diskDb instance that wraps LruDb with backing and Lru cache
+ * const diskDb = new DiskDb();
+ *
+ * // Creat txDb instance that uses diskDb
+ * const txDb = new TransactionDb(diskDb);
+ *
+ * // Open the disk db backing database. Clears the Lru cache
+ * diskDb.open();
+ *
+ * // Declare key/value pair to allocate to store under a the key
+ * const key = u8aFromString('key');
+ * const value = u8aFromString('some value');
+ *
+ * // Store key/value pair in disk db backing and also in Lru cache
+ * diskDb.put(key, value);
+ *
+ * // Retrieve value for key from disk db. Returns cached value if key
+ * // in cache, otherwise returns backing value for key and stores
+ * // this latest retrieved key/value pair in Lru cache
+ * valueRetrieved = diskDb.get(key);
+ *
+ * // Delete key/value pair from disk db backing and set the key to null in Lru cache
+ * diskDb.del(key);
+ *
+ * // Transaction to Store key/value pair in transaction db
+ * const isTxSuccess = txDb.transaction(() => {
+ *   txDb.put(key, value);
+ *
+ *   // Boolean to indicate whether transaction was successful or not
+ *   return true;
+ * });
+ *
+ * console.log(`Transaction successful?: ${isTxSuccess}`);
+ *
+ * // Retrieve transaction value from diskDb db
+ * console.log(`Transfered value: ${diskDb.get(key)}`);
+ *
+ * // Close the diskDb database and clear Lru cache
+ * diskDb.close();
+ * ```
+ */
 export default class DiskDb extends TransactionDb {
   constructor (base: string, name: string, options?: BaseDbOptions) {
     super(
--- packages/db/src/Memory.ts
+++ packages/db/src/Memory.ts
@@ -7,6 +7,62 @@ import { BaseDbOptions } from './types';
 import MemoryDb from './engines/MemoryDb';
 import TransactionDb from './engines/TransactionDb';
 
+/**
+ * @name MemoryDb
+ * @summary Creates a MemoryDb database extending TransactionDb
+ * @description Create an instance of a Memory database using
+ * MemoryDb and extending TransactionDb.
+ * `options` argument (optional) used to specifies whether
+ * to use compression.
+ * - Enable compression when storing "blocks" since they compress well.
+ * - Disable compression when storing "state" Transactions Trie
+ * using @polkadot/trie-db or @polkadot/db/src/engines/TransactionDb.ts
+ * (potentially LevelDb or RocksDb implementation of a Merkle Patricia Trie)
+ * data structure serialised with Recursive Length Prefix (RLP) encoding
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * import u8aFromString from '@polkadot/util/u8a/fromString';
+ * import MemoryDb from '@polkadot/db/Memory';
+ * import TransactionDb from '@polkadot/db/engines/TransactionDb';
+ *
+ * const memoryDb = new MemoryDb();
+ * const txDb = new TransactionDb(memoryDb);
+ *
+ * // Open the memory database
+ * memoryDb.open();
+ *
+ * // Declare key/value pair to allocate to store under a the key
+ * const key = u8aFromString('key');
+ * const value = u8aFromString('some value');
+ *
+ * // Store key/value pair in memory db
+ * memoryDb.put(key, value);
+ *
+ * // Retrieve value for key from memory db
+ * valueRetrieved = memoryDb.get(key);
+ *
+ * // Delete key/value pair from memory db
+ * memoryDb.del(key);
+ *
+ * // Transaction to Store key/value pair in transaction db
+ * const isTxSuccess = txDb.transaction(() => {
+ *   txDb.put(key, value);
+ *
+ *   // Boolean to indicate whether transaction was successful or not
+ *   return true;
+ * });
+ *
+ * console.log(`Transaction successful?: ${isTxSuccess}`);
+ *
+ * // Retrieve transaction value from memory db
+ * console.log(`Transfered value: ${memoryDb.get(key)}`);
+ *
+ * // Close the memory database
+ * memoryDb.close();
+ * ```
+ */
 export default class Memory extends TransactionDb {
   constructor (options?: BaseDbOptions) {
     super(new MemoryDb(options));
