69e066d726191c69d28921bada22aca36ff7e445
Luke Schoen <ltfschoen@gmail.com>
2018-10-17 23:16:35 +0200

    docs: Add Typedoc to is functions

--- packages/trie-db/src/util/is.ts
+++ packages/trie-db/src/util/is.ts
@@ -8,22 +8,52 @@ import isNull from '@polkadot/util/is/null';
 
 import { getNodeType } from './node';
 
+/**
+ * @name isEmptyNode
+ * @signature isEmptyNode (node: Node): node is NodeEmpty
+ * @summary Returns true if node is NULL
+ * @description Refer to [Merkle Patricia Trie specification](https://github.com/ethereum/wiki/wiki/Patricia-Tree#optimization)
+ */
 export function isEmptyNode (node: Node): node is NodeEmpty {
   return isNull(node);
 }
 
+/**
+ * @name isKvNode
+ * @signature isKvNode (node: Node): node is NodeKv
+ * @summary Returns true if node is not empty and contains a single key/value pair
+ * @description Refer to [Merkle Patricia Trie specification](https://github.com/ethereum/wiki/wiki/Patricia-Tree#optimization)
+ */
 export function isKvNode (node: Node): node is NodeKv {
   return !isEmptyNode(node) && node.length === 2;
 }
 
+/**
+ * @name isExtensionNode
+ * @signature isExtensionNode (node: Node): node is NodeKv
+ * @summary Returns true if node is an Extension 2-item node
+ * @description Refer to [Merkle Patricia Trie specification](https://github.com/ethereum/wiki/wiki/Patricia-Tree#optimization)
+ */
 export function isExtensionNode (node: Node): node is NodeKv {
   return getNodeType(node) === NodeType.EXTENSION;
 }
 
+/**
+ * @name isLeafNode
+ * @signature isLeafNode (node: Node): node is NodeKv
+ * @summary Returns true if node is an Leaf 2-item node
+ * @description Refer to [Merkle Patricia Trie specification](https://github.com/ethereum/wiki/wiki/Patricia-Tree#optimization)
+ */
 export function isLeafNode (node: Node): node is NodeKv {
   return getNodeType(node) === NodeType.LEAF;
 }
 
+/**
+ * @name isBranchNode
+ * @signature isBranchNode (node: Node): node is NodeKv
+ * @summary Returns true if node is an Branch 17-item node
+ * @description Refer to [Merkle Patricia Trie specification](https://github.com/ethereum/wiki/wiki/Patricia-Tree#optimization)
+ */
 export function isBranchNode (node: Node): node is NodeBranch {
   return !isEmptyNode(node) && node.length === 17;
 }
