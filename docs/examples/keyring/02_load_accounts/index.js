ece7e1a17910633bc621e98ced7308339c4f09a6
Luke Schoen <ltfschoen@gmail.com>
2018-10-18 15:50:26 +0200

    fix: Update keyring example 02 load accounts not to use addFromAddress

--- docs/examples/keyring/02_load_accounts/index.js
+++ docs/examples/keyring/02_load_accounts/index.js
@@ -3,20 +3,19 @@ const Keyring = require('@polkadot/keyring').default;
 const stringToU8a = require('@polkadot/util/string/toU8a').default;
 
 async function main () {
-  // Create account seed for Alice
+  // Create account seed for Alice and Bob
   const ALICE_SEED = 'Alice'.padEnd(32, ' ');
-
-  // Create address for Bob
-  const BOB_ADDR = '5Gw3s7q4QLkSWwknsiPtjujPv3XM4Trxi5d4PgKMMk3gfGTE';
+  const BOB_SEED = 'Bob'.padEnd(32, ' ');
 
   // Create an instance of the Keyring
   const keyring = new Keyring();
 
-  // Create Alice's pair with account seed. Add to keyring pair dictionary
+  // Create their pairs with account seeds. Add to keyring pair dictionary
   keyring.addFromSeed(stringToU8a(ALICE_SEED));
+  keyring.addFromSeed(stringToU8a(BOB_SEED));
 
-  // Create Bob's pair with account address. Add to keyring pair dictionary
-  keyring.addFromAddress(BOB_ADDR);
+  // Note that we did not use `addFromAddress` since it does not add the
+  // secret key to memory so we cannot later retrieve it with `getPairs`
 
   // Retrieve all keyring pairs from the keyring pair dictionary
   keyring
