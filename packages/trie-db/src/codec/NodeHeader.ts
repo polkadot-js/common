a1090fbb39c87d29c338f12237ea45848c12efa8
Jaco Greeff <jacogr@gmail.com>
2018-11-14 18:13:59 +0100

    NodeHeader encoding/decoding

--- packages/trie-db/src/codec/NodeHeader.ts
+++ packages/trie-db/src/codec/NodeHeader.ts
@@ -4,33 +4,10 @@
 
 import { EnumType } from '@polkadot/types/codec';
 import { bool as Bool, u8 as U8, u64 as U64 } from '@polkadot/types';
-import { isU8a } from '@polkadot/util/index';
 
-import { BRANCH_NODE_NO_VALUE, BRANCH_NODE_WITH_VALUE } from './constants';
+import { BRANCH_NODE_NO_VALUE, BRANCH_NODE_WITH_VALUE, EMPTY_TRIE, EXTENSION_NODE_BIG, EXTENSION_NODE_OFFSET, EXTENSION_NODE_SMALL_MAX, EXTENSION_NODE_THRESHOLD, LEAF_NODE_BIG, LEAF_NODE_OFFSET, LEAF_NODE_SMALL_MAX, LEAF_NODE_THRESHOLD } from './constants';
 
 class Branch extends Bool {
-  constructor (value: Bool | Uint8Array | boolean) {
-    super(
-      Branch.decodeBranch(value)
-    );
-  }
-
-  private static decodeBranch (value: Bool | Uint8Array | boolean): boolean {
-    if (value instanceof Bool) {
-      return value.valueOf();
-    } else if (isU8a(value)) {
-      if (value[0] === BRANCH_NODE_NO_VALUE) {
-        return false;
-      } else if (value[0] === BRANCH_NODE_WITH_VALUE) {
-        return true;
-      }
-
-      throw new Error(`Unexpected branch value ${value[0]}`);
-    }
-
-    return value;
-  }
-
   toU8a (isBare?: boolean): Uint8Array {
     return new Uint8Array(
       this.valueOf() === true
@@ -41,11 +18,33 @@ class Branch extends Bool {
 }
 
 export class Extension extends U64 {
+  toU8a (isBare?: boolean): Uint8Array {
+    const nibbleCount = this.toNumber();
 
+    if (nibbleCount < EXTENSION_NODE_THRESHOLD) {
+      return new Uint8Array([EXTENSION_NODE_OFFSET + nibbleCount]);
+    }
+
+    return new Uint8Array([
+      EXTENSION_NODE_BIG,
+      nibbleCount - EXTENSION_NODE_THRESHOLD
+    ]);
+  }
 }
 
 export class Leaf extends U64 {
+  toU8a (isBare?: boolean): Uint8Array {
+    const nibbleCount = this.toNumber();
 
+    if (nibbleCount < LEAF_NODE_THRESHOLD) {
+      return new Uint8Array([LEAF_NODE_OFFSET + nibbleCount]);
+    }
+
+    return new Uint8Array([
+      LEAF_NODE_BIG,
+      nibbleCount - LEAF_NODE_THRESHOLD
+    ]);
+  }
 }
 
 export class Null extends U8 {
@@ -54,13 +53,37 @@ export class Null extends U8 {
   }
 }
 
-export default class NodeHeader extends EnumType<Null> {
-  constructor (value: any) {
+export default class NodeHeader extends EnumType<Null | Branch | Extension | Leaf> {
+  constructor (input: any) {
+    const [index, value] = NodeHeader.decodeNodeHeader(input);
+
     super([
-      Null,
-      Branch,
-      Extension,
-      Leaf
-    ]);
+      Null, // 0
+      Branch, // 1
+      Extension, // 2
+      Leaf // 3
+    ], value, index);
+  }
+
+  private static decodeNodeHeader (input: Uint8Array): [number, Null | Branch | Extension | Leaf] {
+    const firstByte = input[0];
+
+    if (firstByte === EMPTY_TRIE) {
+      return [0, new Null()];
+    } else if (firstByte >= LEAF_NODE_OFFSET && firstByte <= LEAF_NODE_SMALL_MAX) {
+      return [3, new Leaf(firstByte - LEAF_NODE_OFFSET)];
+    } else if (firstByte === LEAF_NODE_BIG) {
+      return [3, new Leaf(input[1] + LEAF_NODE_THRESHOLD)];
+    } else if (firstByte >= EXTENSION_NODE_OFFSET && firstByte <= EXTENSION_NODE_SMALL_MAX) {
+      return [2, new Extension(input[1] - EXTENSION_NODE_OFFSET)];
+    } else if (firstByte === EXTENSION_NODE_BIG) {
+      return [2, new Extension(input[1] + EXTENSION_NODE_THRESHOLD)];
+    } else if (firstByte === BRANCH_NODE_NO_VALUE) {
+      return [1, new Branch(false)];
+    } else if (firstByte === BRANCH_NODE_WITH_VALUE) {
+      return [1, new Branch(true)];
+    }
+
+    throw new Error('Unreachable');
   }
 }
