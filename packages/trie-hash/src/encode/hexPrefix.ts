3bc1b7541ba436c7b767d8aed143a8f36e5664ce
Jaco Greeff <jacogr@gmail.com>
2018-11-21 12:26:38 +0100

    LICENSE and README updates (#254)
    * LICENSE and README updates
    
    * Remove duplicated definition
    
    * Remove duplicated file

--- packages/trie-hash/src/encode/hexPrefix.ts
+++ packages/trie-hash/src/encode/hexPrefix.ts
@@ -1,6 +1,6 @@
 // Copyright 2017-2018 @polkadot/trie-hash authors & contributors
 // This software may be modified and distributed under the terms
-// of the ISC license. See the LICENSE file for details.
+// of the Apache-2.0 license. See the LICENSE file for details.
 
 export default function encodeHexPrefix (nibbles: Uint8Array | Array<number>, isLeaf: boolean): Uint8Array {
   const bits = ((nibbles.length & 1) + (2 * (isLeaf ? 1 : 0))) << 4;
