30bf357d5d915c82708400ef5fa631a847ee7b83
Jaco Greeff <jacogr@gmail.com>
2018-12-17 21:24:02 +0100

    React native support, strengthen TextEncoder/Decoder (#273)

--- packages/util/src/string/polyfill/textDecoder.ts
+++ packages/util/src/string/polyfill/textDecoder.ts
@@ -5,17 +5,9 @@
 // tslint:disable-next-line
 if (typeof TextDecoder === 'undefined') {
   try {
-    // @ts-ignore For the Node.js case
+    // @ts-ignore
     global.TextDecoder = require('util').TextDecoder;
   } catch (error) {
-    // @ts-ignore For the old browser case
-    global.TextDecoder = {
-      // Very naive ascii-only decoder
-      decode: (u8a: Uint8Array) => {
-        return u8a.reduce((str, code) => {
-          return str + String.fromCharCode(code);
-        }, '');
-      }
-    };
+    // noop
   }
 }
--- packages/util/src/string/toU8a.ts
+++ packages/util/src/string/toU8a.ts
@@ -4,7 +4,13 @@
 
 import './polyfill/textEncoder';
 
-const encoder = new TextEncoder();
+let encoder: TextEncoder | undefined;
+
+try {
+  encoder = new TextEncoder();
+} catch (error) {
+  // noop
+}
 
 /**
  * @name stringToU8a
@@ -24,7 +30,15 @@ const encoder = new TextEncoder();
 export default function stringToU8a (value?: string): Uint8Array {
   if (!value) {
     return new Uint8Array([]);
+  } else if (encoder) {
+    return encoder.encode(value);
   }
 
-  return encoder.encode(value);
+  const u8a = new Uint8Array(value.length);
+
+  for (let i = 0; i < value.length; i++) {
+    u8a[i] = value.charCodeAt(i);
+  }
+
+  return u8a;
 }
