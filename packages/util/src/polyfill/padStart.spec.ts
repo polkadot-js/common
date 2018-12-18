6d8e78840b1bce18526ee3756b6a6c432bf956f7
Jaco Greeff <jacogr@gmail.com>
2018-12-18 15:15:10 +0100

    Expand and organise polyfills (#276)

--- packages/util/src/polyfill/padStart.spec.ts
+++ packages/util/src/polyfill/padStart.spec.ts
@@ -2,28 +2,22 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-describe('padding', () => {
-  let stringEnd: (length: number, fill?: string) => string;
+describe('String padStart', () => {
   let stringStart: (length: number, fill?: string) => string;
 
   beforeEach(() => {
-    stringEnd = String.prototype.padEnd;
     stringStart = String.prototype.padStart;
 
-    String.prototype.padEnd = null as any;
     String.prototype.padStart = null as any;
 
-    require('./padding');
+    require('./padStart');
   });
 
   afterEach(() => {
-    String.prototype.padEnd = stringEnd;
     String.prototype.padStart = stringStart;
   });
 
   it('does padding', () => {
-    expect('test'.padEnd(8, 'A')).toEqual('testAAAA');
-    expect('test'.padEnd(8)).toEqual('test    ');
     expect('test'.padStart(8, 'A')).toEqual('AAAAtest');
     expect('test'.padStart(8)).toEqual('    test');
   });
