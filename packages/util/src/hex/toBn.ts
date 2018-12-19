0e30c483790e3dc01a42e8a116ab977738fb1347
Amaury Martiny <amaury.martiny@protonmail.com>
2018-12-19 18:43:44 +0100

    Add conversion to negative BN's (#277)
    * Support negative number hexToBn
    
    * Fix u8aToBn
    
    * Update signature
    
    * Update params description

--- packages/util/src/hex/toBn.ts
+++ packages/util/src/hex/toBn.ts
@@ -4,7 +4,9 @@
 
 import BN from 'bn.js';
 
+import isBoolean from '../is/boolean';
 import hexStripPrefix from './stripPrefix';
+import { ToBnOptions } from '../types';
 
 function reverse (value: string): string {
   return (value.match(new RegExp('.{1,2}', 'g')) || [])
@@ -14,10 +16,14 @@ function reverse (value: string): string {
 
 /**
  * @name hexToBn
- * @signature hexToBn (value?: string, isLe: boolean = false): BN
+ * @signature hexToBn (value?: string, _options: ToBnOptions | boolean = { isLe: false, isNegative: false }): BN
  * @summary Creates a BN.js bignumber object from a hex string.
  * @description
  * `null` inputs returns a `BN(0)` result. Hex input values return the actual value converted to a BN. Anything that is not a hex string (including the `0x` prefix) throws an error.
+ * @param _value The value to convert
+ * @param _options Options to pass while converting
+ * @param _options.isLe Convert using Little Endian
+ * @param _options.isNegative Convert using two's complement
  * @example
  * <BR>
  *
@@ -27,12 +33,23 @@ function reverse (value: string): string {
  * hexToBn('0x123480001f'); // => BN(0x123480001f)
  * ```
  */
-export default function hexToBn (_value?: string, isLe: boolean = false): BN {
+export default function hexToBn (
+  _value?: string | number | null,
+  _options: ToBnOptions | boolean = { isLe: false, isNegative: false }
+): BN {
   if (!_value) {
     return new BN(0);
   }
 
-  const value = hexStripPrefix(_value);
+  const options: ToBnOptions = isBoolean(_options)
+    ? { isLe: _options, isNegative: false }
+    : _options;
 
-  return new BN((isLe ? reverse(value) : value) || '00', 16);
+  const value = hexStripPrefix(_value as string);
+
+  // FIXME: Use BN's 3rd argument `isLe` once this issue is fixed
+  // https://github.com/indutny/bn.js/issues/208
+  const bn = new BN((options.isLe ? reverse(value) : value) || '00', 16);
+
+  return options.isNegative ? bn.fromTwos(8) : bn;
 }
--- packages/util/src/u8a/toBn.ts
+++ packages/util/src/u8a/toBn.ts
@@ -6,13 +6,18 @@ import BN from 'bn.js';
 
 import hexToBn from '../hex/toBn';
 import u8aToHex from './toHex';
+import { ToBnOptions } from '../types';
 
 /**
  * @name u8aToBn
- * @signature u8aToHex (value?: Uint8Array, isLe: boolean = false): BN
+ * @signature u8aToHex (value?: Uint8Array, options: ToBnOptions | boolean = true): BN
  * @summary Creates a BN from a Uint8Array object.
  * @description
  * `UInt8Array` input values return the actual BN. `null` or `undefined` values returns an `0x0` value.
+ * @param value The value to convert
+ * @param options Options to pass while converting
+ * @param options.isLe Convert using Little Endian
+ * @param options.isNegative Convert using two's complement
  * @example
  * <BR>
  *
@@ -22,9 +27,9 @@ import u8aToHex from './toHex';
  * u8aToHex(new Uint8Array([0x68, 0x65, 0x6c, 0x6c, 0xf])); // 0x68656c0f
  * ```
  */
-export default function u8aToBn (value: Uint8Array, isLe: boolean): BN {
+export default function u8aToBn (value: Uint8Array, options: ToBnOptions | boolean = true): BN {
   return hexToBn(
     u8aToHex(value),
-    isLe
+    options
   );
 }
