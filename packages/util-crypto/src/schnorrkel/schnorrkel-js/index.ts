6bda72dea13a9f14cb82ccebcb0e23e344037da8
Jaco Greeff <jacogr@gmail.com>
2019-02-14 14:55:31 +0100

    schnorrkel imports from @parity namespace

--- packages/util-crypto/src/schnorrkel/schnorrkel-js/index.ts
+++ packages/util-crypto/src/schnorrkel/schnorrkel-js/index.ts
@@ -1,3 +1,8 @@
+// Copyright 2017-2019 @polkadot/util-crypto authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
 import './polyfill';
 
+// export * from '@parity/schnorrkel-js/schnorrkel_js';
 export * from './schnorrkel_js';
