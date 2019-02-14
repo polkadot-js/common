a78c899a70367a297a69171a2852287625e73c86
Jaco Greeff <jacogr@gmail.com>
2019-02-14 17:49:34 +0100

    Update index.spec.ts

--- packages/keyring/src/index.spec.ts
+++ packages/keyring/src/index.spec.ts
@@ -8,7 +8,7 @@ import Keyring from './index';
 import setPrefix from './address/setPrefix';
 
 describe('keypair', () => {
-  describe.skip('ed25519', () => {
+  describe('ed25519', () => {
     const publicKeyOne = new Uint8Array([47, 140, 97, 41, 216, 22, 207, 81, 195, 116, 188, 127, 8, 195, 230, 62, 209, 86, 207, 120, 174, 251, 74, 101, 80, 217, 123, 135, 153, 121, 119, 238]);
     const publicKeyTwo = new Uint8Array([215, 90, 152, 1, 130, 177, 10, 183, 213, 75, 254, 211, 201, 100, 7, 58, 14, 225, 114, 243, 218, 166, 35, 37, 175, 2, 26, 104, 247, 7, 81, 26]);
     const seedOne = stringToU8a('12345678901234567890123456789012');
