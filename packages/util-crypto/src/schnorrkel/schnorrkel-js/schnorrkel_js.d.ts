8a82945d6f3bc95e34fa3fda501414e8ea8bed57
Jaco Greeff <jacogr@gmail.com>
2019-02-15 16:51:55 +0100

    Update with scripted schnorrkel-js

--- packages/util-crypto/src/schnorrkel/schnorrkel-js/schnorrkel_js.d.ts
+++ packages/util-crypto/src/schnorrkel/schnorrkel-js/schnorrkel_js.d.ts
@@ -3,5 +3,7 @@ export function sign(arg0: Uint8Array, arg1: Uint8Array, arg2: Uint8Array): Uint
 export function verify(arg0: Uint8Array, arg1: Uint8Array, arg2: Uint8Array): boolean;
 export function secret_from_seed(arg0: Uint8Array): Uint8Array;
 export function keypair_from_seed(arg0: Uint8Array): Uint8Array;
+
 export function isReady(): boolean;
 export function waitReady(): Promise<boolean>;
+
--- packages/util-crypto/src/schnorrkel/schnorrkel-js/schnorrkel_js_bg.js
+++ packages/util-crypto/src/schnorrkel/schnorrkel-js/schnorrkel_js_bg.js
@@ -1,11 +1,12 @@
+
 const bytes = require('./schnorrkel_js_wasm');
 
-module.exports = function init (schnorrkel_js) {
+module.exports = function createExportPromise (wasmImports) {
   const imports = {
-    './schnorrkel_js': schnorrkel_js
+    './schnorrkel_js': wasmImports
   };
 
   return WebAssembly
     .instantiate(bytes, imports)
     .then((wasm) => wasm.instance.exports);
-};
+}
