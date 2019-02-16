b15abe1167a37ff6b46371ab063ecc42a39c426f
Jaco Greeff <jacogr@gmail.com>
2019-02-16 12:09:07 +0100

    null on WASM non-available (#317)

---
+++ packages/util-crypto/src/schnorrkel/ready.spec.ts
@@ -0,0 +1,25 @@
+// Copyright 2017-2019 @polkadot/util-crypto authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+describe('ready', () => {
+  let globalWA: typeof WebAssembly;
+
+  beforeEach(() => {
+    globalWA = (global as any).WebAssembly;
+    (global as any).WebAssembly = null;
+  });
+
+  afterEach(() => {
+    (global as any).WebAssembly = globalWA;
+  });
+
+  it('returns false when Webassembly is not available', async () => {
+    (global as any).WebAssembly = null;
+
+    const schnorrkelWaitReady = require('./ready').default;
+    const isReady = await schnorrkelWaitReady();
+
+    expect(isReady).toBe(false);
+  });
+});
