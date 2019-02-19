e19d377eecf169d26093375d1f1ad7d8e8556488
Jaco Greeff <jacogr@gmail.com>
2019-02-19 16:02:45 +0100

    Remove test polyfill, only include in tests (#322)
    * Remove test polyfill, only include in tests
    
    * Test polyfill for keyring

--- packages/keyring/src/index.spec.ts
+++ packages/keyring/src/index.spec.ts
@@ -2,6 +2,8 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import '@polkadot/util-crypto/schnorrkel/test-polyfill';
+
 import { hexToU8a, stringToU8a } from '@polkadot/util/index';
 import { cryptoWaitReady } from '@polkadot/util-crypto/index';
 
