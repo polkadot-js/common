207f987bb5da2babc7ea4eaefa7ba48e955f9096
Jaco Greeff <jacogr@gmail.com>
2019-02-26 09:43:05 +0100

    Cleanup old

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,16 @@
 module.exports = {
+  base: '/common/',
   title: 'polkadot-js/common',
   description: 'Utilities and libraries in-use accross @polkadot projects',
   themeConfig: {
     displayAllHeaders: true,
     lastUpdated: 'Last Updated',
+    nav: [
+      { text: 'GitHub', link: 'https://github.com/polkadot-js/common' }
+    ],
     sidebar: [
-      'util',
-      'keyring'
+      '/util',
+      '/keyring'
     ]
   }
 };
