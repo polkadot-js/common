b9ac91839d41aa0893a1f9aa33f978b16b85a90c
Jaco Greeff <jacogr@gmail.com>
2019-02-27 14:32:30 +0100

    Drop BBQ spec (only used in tests) (#336)

---
+++ packages/chainspec/src/index.ts
@@ -0,0 +1,6 @@
+// Copyright 2017-2019 @polkadot/chainspec authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+export { default as alexander } from './alexander';
+export { default as drieddanta } from './drieddanta';
--- packages/trie-hash/src/genesis.spec.ts
+++ packages/trie-hash/src/genesis.spec.ts
@@ -2,15 +2,13 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import bbqbirch from '@polkadot/chainspec/bbqbirch';
+import { alexander, drieddanta } from '@polkadot/chainspec/index';
 import { hexToU8a, u8aToHex } from '@polkadot/util/index';
 
 import trieRoot from './trieRoot';
 
 describe('genesis roots', () => {
-  [
-    bbqbirch
-  ].forEach(({ genesis: { raw }, genesisRoot, name }) => {
+  [alexander, drieddanta].forEach(({ genesis: { raw }, genesisRoot, name }) => {
     it(`has the correct root for ${name}`, () => {
       expect(
         u8aToHex(
