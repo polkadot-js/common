77f8399b891a227bd54abd638630e2741ee22e39
Jaco Greeff <jacogr@gmail.com>
2019-03-21 10:30:50 +0100

    Utility function to extract from path only

--- packages/util-crypto/src/key/index.ts
+++ packages/util-crypto/src/key/index.ts
@@ -5,7 +5,8 @@
 /**
  * @summary Create keys from paths, seeds and password
  */
-export { default as keyExtract } from './extract';
+export { default as keyExtractPath } from './extractPath';
+export { default as keyExtractSuri } from './extractSuri';
 export { default as keyFromPath } from './fromPath';
 export { default as keyHdkdEd25519 } from './hdkdEd25519';
 export { default as keyHdkdSr25519 } from './hdkdEd25519';
