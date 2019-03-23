2bd305ee9efb0e12b198eb6d75be601e7be605a9
Jaco Greeff <jacogr@gmail.com>
2019-03-23 18:21:09 +0100

    Bump wasm-crypto for bip39ToSeed

--- packages/util-crypto/src/mnemonic/toSecret.spec.ts
+++ packages/util-crypto/src/mnemonic/toSecret.spec.ts
@@ -2,14 +2,14 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { u8aToHex } from '@polkadot/util';
+
 import toSecret from './toSecret';
 
 describe('mnemonicToSecret', () => {
   it('generates a valid seed', () => {
     expect(
-      toSecret('basket actual')
-    ).toEqual(new Uint8Array([
-      92, 242, 212, 168, 176, 53, 94, 144, 41, 91, 223, 197, 101, 160, 34, 164, 9, 175, 6, 61, 83, 101, 187, 87, 191, 116, 217, 82, 143, 73, 75, 250, 68, 0, 245, 61, 131, 73, 184, 15, 218, 228, 64, 130, 215, 249, 84, 30, 29, 186, 43, 0, 59, 207, 236, 157, 13, 83, 120, 28, 166, 118, 101, 31
-    ]));
+      u8aToHex(toSecret('seed sock milk update focus rotate barely fade car face mechanic mercy'))
+    ).toEqual('0x3c121e20de068083b49c2315697fb59a2d9e8643c24e5ea7628132c58969a0275693dd5bd9d4cc9e648475eba9613ed4678f4d62560a9c42f75bac04022ded25');
   });
 });
