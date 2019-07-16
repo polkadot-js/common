6c794626e8b5d2c693ead0b82bb4129f35c2f4c8
Jaco Greeff <jacogr@gmail.com>
2019-07-16 20:09:03 +0200

    Allow Kusama network prefix (#441)
    * Allow Kusama network prefix
    
    * Allow deprecated 68

--- packages/util-crypto/src/address/types.ts
+++ packages/util-crypto/src/address/types.ts
@@ -2,4 +2,9 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-export type Prefix = 0 | 1 | 3 | 42 | 43 | 68 | 69;
+type PrefixPolkadot = 0;
+type PrefixKusama = 2;
+type PrefixSubstrate = 42;
+type DeprecatedPrefixBBQ = 68;
+
+export type Prefix = PrefixPolkadot | PrefixKusama | PrefixSubstrate | DeprecatedPrefixBBQ;
