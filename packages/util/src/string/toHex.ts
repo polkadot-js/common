f68c93f802b6f59c74c6ce449f8ee4918ad65496
Jaco Greeff <jacogr@gmail.com>
2019-09-11 12:19:00 +0200

    string <-> hex conversion utilities

---
+++ packages/util/src/string/toHex.ts
@@ -0,0 +1,26 @@
+// Copyright 2017-2019 @polkadot/util authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import u8aToHex from '../u8a/toHex';
+import stringToU8a from './toU8a';
+
+/**
+ * @name stringToHex
+ * @summary Creates a hex string from a utf-8 string
+ * @description
+ * String input values return the actual encoded hex value.
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * import { stringToHex } from '@polkadot/util';
+ *
+ * stringToU8a('hello'); // 0x68656c6c6f
+ * ```
+ */
+export default function stringToHex (value?: string): string {
+  return u8aToHex(
+    stringToU8a(value)
+  );
+}
