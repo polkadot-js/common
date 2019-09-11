34f8b40a81cf9071141e57db6b0c2421d02f93a0
Jaco Greeff <jacogr@gmail.com>
2019-09-11 12:25:04 +0200

    Add tests

---
+++ packages/util/src/string/toHex.spec.ts
@@ -0,0 +1,19 @@
+// Copyright 2017-2019 @polkadot/util authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { stringToHex } from '.';
+
+describe('hexToString', (): void => {
+  it('converts an empty to ""', (): void => {
+    expect(
+      stringToHex()
+    ).toEqual('0x');
+  });
+
+  it('converts to a hex from string', (): void => {
+    expect(
+      stringToHex('hello')
+    ).toEqual('0x68656c6c6f');
+  });
+});
