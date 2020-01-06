241febb7078c568d982a6d7a4b6e96423909c0a0
Jaco Greeff <jacogr@gmail.com>
2020-01-06 13:43:49 +0100

    Bump deps (#497)

--- packages/util/src/polyfill/setPrototypeOf.spec.ts
+++ packages/util/src/polyfill/setPrototypeOf.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-proto */
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
 /* eslint-disable @typescript-eslint/unbound-method */
-// Copyright 2017-2019 @polkadot/util authors & contributors
+// Copyright 2017-2020 @polkadot/util authors & contributors
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
