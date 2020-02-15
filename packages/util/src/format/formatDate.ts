e6f135bf92acf5baf5054fc45eb3544d58d22eca
Jaco Greeff <jacogr@gmail.com>
2020-02-15 11:21:14 +0100

    Cleanup padding

--- packages/util/src/format/formatDate.ts
+++ packages/util/src/format/formatDate.ts
@@ -2,13 +2,18 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+/** @internal */
+function zeroPad (value: number): string {
+  return value.toString().padStart(2, '0');
+}
+
 export default function formatDate (date: Date): string {
   const year = date.getFullYear().toString();
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const day = date.getDate().toString().padStart(2, '0');
-  const hour = date.getHours().toString().padStart(2, '0');
-  const minute = date.getMinutes().toString().padStart(2, '0');
-  const second = date.getSeconds().toString().padStart(2, '0');
+  const month = zeroPad((date.getMonth() + 1));
+  const day = zeroPad(date.getDate());
+  const hour = zeroPad(date.getHours());
+  const minute = zeroPad(date.getMinutes());
+  const second = zeroPad(date.getSeconds());
 
   return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
 }
