600f9161936d6485c591a5875733bdd12e0a53c9
Jaco Greeff <jacogr@gmail.com>
2020-02-21 16:40:25 +0100

    Add isBigInt & bnToBn support for

---
+++ packages/util/src/is/bigInt.ts
@@ -0,0 +1,21 @@
+// Copyright 2017-2020 @polkadot/util authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+/**
+ * @name isBigInt
+ * @summary Tests for a `BigInt` object instance.
+ * @description
+ * Checks to see if the input object is an instance of `BigInt`
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * import { isBigInt } from '@polkadot/util';
+ *
+ * console.log('isBigInt', isBigInt(123_456n)); // => true
+ * ```
+ */
+export default function isBigInt (value: BigInt | object | string | number | undefined | null): value is BigInt {
+  return typeof value === 'bigint';
+}
