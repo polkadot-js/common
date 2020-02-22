dd8b8b084551232bc20ff3315c84cbf9f0a9682a
Jaco Greeff <jacogr@gmail.com>
2020-02-22 15:18:18 +0100

    Remove stray any, allow never

--- packages/util/src/is/childClass.ts
+++ packages/util/src/is/childClass.ts
@@ -19,8 +19,7 @@ import { Constructor } from '../types';
  * console.log('isChildClass', isChildClass(BN, Uint8Array); // => false
  * ```
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function isChildClass <P extends Constructor> (Parent: P, Child?: Constructor | null | unknown): Child is P {
+export default function isChildClass <P extends Constructor> (Parent: P, Child?: Constructor | null | unknown | never): Child is P {
   // https://stackoverflow.com/questions/30993434/check-if-a-constructor-inherits-another-in-es6/30993664
   return Child
     // eslint-disable-next-line no-prototype-builtins
