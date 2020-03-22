619f216d4c8d339f2af8cdf755ebebebbf342087
Jaco Greeff <jacogr@gmail.com>
2020-03-22 08:00:30 +0100

    wasm-crypto initNone

---
+++ packages/util-crypto/src/initNone.ts
@@ -0,0 +1,11 @@
+// Copyright 2017-2020 @polkadot/util-crypto authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+export function cryptoIsReady (): boolean {
+  return true;
+}
+
+export function cryptoWaitReady (): Promise<boolean> {
+  return Promise.resolve(true);
+}
