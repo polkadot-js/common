d40d6c65f71bac4b62912dc95265f8bb891b4cca
Jaco Greeff <jacogr@gmail.com>
2020-04-07 21:23:20 +0200

    isDevelopment regex

--- packages/util/src/is/testChain.ts
+++ packages/util/src/is/testChain.ts
@@ -2,7 +2,7 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-const re = new RegExp('^(Development|Local Testnet)$');
+const re = new RegExp('(Development|Local Testnet)$');
 
 export default function isTestChain (chain?: string | null): boolean {
   if (!chain) {
