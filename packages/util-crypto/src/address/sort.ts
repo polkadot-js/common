4621353d5b80586b8ba5873a91caa99371400627
Jaco Greeff <jacogr@gmail.com>
2020-04-26 14:28:33 +0200

    Sort with ss58 prefix

--- packages/util-crypto/src/address/sort.ts
+++ packages/util-crypto/src/address/sort.ts
@@ -2,13 +2,15 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
+import { Prefix } from './types';
+
 import { u8aSorted } from '@polkadot/util';
 
 import decodeAddress from './decode';
 import encodeAddress from './encode';
 
-export default function sortAddresses (addresses: (Uint8Array | string)[]): string[] {
+export default function sortAddresses (addresses: (Uint8Array | string)[], ss58Format?: Prefix): string[] {
   return u8aSorted(
     addresses.map((who) => decodeAddress(who))
-  ).map((u8a) => encodeAddress(u8a));
+  ).map((u8a) => encodeAddress(u8a, ss58Format));
 }
