9b75ed91c221f7ee2847381c81c518ac814d5fa4
joepetrowski <joe@parity.io>
2020-04-26 14:31:21 +0200

    sort addresses

--- docs/examples/util-crypto/04_generate_multisig_address/index.js
+++ docs/examples/util-crypto/04_generate_multisig_address/index.js
@@ -1,27 +1,41 @@
 /* eslint-disable header/header */
-import { createKeyMulti, encodeAddress } from '@polkadot/util-crypto';
+import { createKeyMulti, encodeAddress, sortAddresses } from '@polkadot/util-crypto';
+
+const SS58Prefix = 0;
+
+// Input the addresses that will make up the multisig account.
+const addresses = [
+  '1nUC7afqmo7zwRFWxDjrUQu9skk6fk99pafb4SiyGSRc8z3',
+  '1ZX2XntfLEHrBPy73DpfQp9rG7pbLyvrFjEpi7mNKQgyga5',
+  '14b1kB7CrqzRUeMsKc26FJ73f8FCpxAX6sNieu9gfYSfJuoL'
+];
+
+// The number of accounts that must approve. Must be greater than 0 and less than
+// or equal to the total number of addresses.
+const threshold = 2;
+
+// The address (as index in `addresses`) that will submit a transaction.
+const index = 0;
 
 function main () {
-  // Input the addresses that will make up the multisig account. This utility
-  // will sort them for you but when you submit to a Substrate node, you must
-  // sort them yourself.
-  const addresses = [
-    '1nUC7afqmo7zwRFWxDjrUQu9skk6fk99pafb4SiyGSRc8z3',
-    '1ZX2XntfLEHrBPy73DpfQp9rG7pbLyvrFjEpi7mNKQgyga5',
-    '14b1kB7CrqzRUeMsKc26FJ73f8FCpxAX6sNieu9gfYSfJuoL'
-  ];
-
-  // The number of accounts that must approve. Must be greater than 0 and less than
-  // or equal to the total number of addresses.
-  const threshold = 2;
 
   // Address as a byte array.
   const multiAddress = createKeyMulti(addresses, threshold);
 
-  // Convert byte array to SS58 encoding. Second arg is SS58 prefix.
-  const Ss58Address = encodeAddress(multiAddress, 0);
+  // Convert byte array to SS58 encoding.
+  const Ss58Address = encodeAddress(multiAddress, SS58Prefix);
 
-  console.log(`Multisig Address: ${Ss58Address}`);
+  console.log(`\nMultisig Address: ${Ss58Address}`);
+
+  // Take addresses and remove the sender.
+  const otherSignatories = addresses.filter(who => who !== addresses[index]);
+
+  // Sort them by public key.
+  const otherSignatoriesSorted = sortAddresses(otherSignatories).map(
+    function(e) { e = encodeAddress(e, SS58Prefix); return e; }
+  );
+
+  console.log(`\nOther Signatories: ${otherSignatoriesSorted}\n`);
 
   process.exit();
 }
