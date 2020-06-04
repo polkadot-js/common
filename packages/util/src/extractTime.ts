1447445bb0b7486a242184a18d3ac14d20a62590
Keith Ingram <kwingram25@gmail.com>
2020-06-04 21:59:56 +1200

    lint

--- packages/util/src/extractTime.ts
+++ packages/util/src/extractTime.ts
@@ -12,7 +12,6 @@ const DAY = HRS * 24;
  * @summary Add together two Time arrays
  */
 
-
 function addTime (a: Time, b: Time): Time {
   return [a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3]];
 }
