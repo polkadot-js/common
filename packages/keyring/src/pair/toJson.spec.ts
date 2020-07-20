901a27f8f71bfd57e15a533d282b46c6fc642fb0
Jaco Greeff <jacogr@gmail.com>
2020-07-20 23:04:52 +0200

    Update for base64 encoding

--- packages/keyring/src/pair/toJson.spec.ts
+++ packages/keyring/src/pair/toJson.spec.ts
@@ -12,7 +12,7 @@ describe('toJson', (): void => {
       keyring.alice.toJson()
     ).toMatchObject({
       address: '5GoKvZWG5ZPYL1WUovuHW3zJBWBP5eT8CbqjdRY4Q6iMaQua',
-      encoded: '0x3053020101300506032b657004220420416c696365202020202020202020202020202020202020202020202020202020d172a74cda4c865912c32ba0a80a57ae69abae410e5ccb59dee84e2f4432db4fa123032100d172a74cda4c865912c32ba0a80a57ae69abae410e5ccb59dee84e2f4432db4f',
+      encoded: 'MFMCAQEwBQYDK2VwBCIEIEFsaWNlICAgICAgICAgICAgICAgICAgICAgICAgICAg0XKnTNpMhlkSwyugqApXrmmrrkEOXMtZ3uhOL0Qy20+hIwMhANFyp0zaTIZZEsMroKgKV65pq65BDlzLWd7oTi9EMttP',
       encoding: {
         content: ['pkcs8', 'ed25519'],
         type: ['none'],
@@ -28,7 +28,7 @@ describe('toJson', (): void => {
   it('creates an encoded output with passphrase', (): void => {
     const json = keyring.alice.toJson('testing');
 
-    expect(json.encoded).toHaveLength(252 + ((32 + 32 + 12) * 2));
+    expect(json.encoded).toHaveLength(268);
     expect(json).toMatchObject({
       address: '5GoKvZWG5ZPYL1WUovuHW3zJBWBP5eT8CbqjdRY4Q6iMaQua',
       encoding: {
