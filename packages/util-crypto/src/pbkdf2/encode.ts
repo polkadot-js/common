c8919a2a0ef73c3b493f262e7f541e89f465c0f9
Jaco Greeff <jacogr@gmail.com>
2020-07-20 12:16:35 +0200

    pbkdf2 from wasm

--- packages/util-crypto/src/pbkdf2/encode.ts
+++ packages/util-crypto/src/pbkdf2/encode.ts
@@ -4,6 +4,7 @@
 
 import { pbkdf2Sync } from 'pbkdf2';
 import { bufferToU8a, u8aToBuffer, u8aToU8a } from '@polkadot/util';
+import { isReady, pbkdf2 } from '@polkadot/wasm-crypto';
 
 import randomAsU8a from '../random/asU8a';
 
@@ -12,10 +13,11 @@ interface Result {
   salt: Uint8Array;
 }
 
-export default function pbkdf2Encode (passphrase?: Uint8Array | string, salt = randomAsU8a(), rounds = 64, length = 2048): Result {
-  const password = bufferToU8a(
-    pbkdf2Sync(u8aToBuffer(u8aToU8a(passphrase)), u8aToBuffer(salt), rounds, length)
-  );
+export default function pbkdf2Encode (passphrase?: Uint8Array | string, salt = randomAsU8a(), rounds = 2048): Result {
+  const u8a = u8aToU8a(passphrase);
+  const password = isReady()
+    ? pbkdf2(u8a, salt, rounds)
+    : bufferToU8a(pbkdf2Sync(u8aToBuffer(u8a), u8aToBuffer(salt), rounds, 64));
 
   return { password, salt };
 }
