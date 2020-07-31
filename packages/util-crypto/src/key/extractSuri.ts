d84e310207a0cfe18fc8095e6da4c0e1ce48b35b
Brad Larson <brad@polychain.capital>
2020-07-31 13:38:48 +0100

    [FIX] Adjust error message
    I ran into a case where the secret value provided had trailing
    whitespace, and when the code ran, it logged that secret value as part
    of the stack trace.
    
    This change will make it harder for users to accidentally leak key
    material.

--- packages/util-crypto/src/key/extractSuri.ts
+++ packages/util-crypto/src/key/extractSuri.ts
@@ -22,7 +22,7 @@ export default function keyExtract (suri: string): ExtractResult {
   // eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
   const matches = suri.match(RE_CAPTURE);
 
-  assert(!isNull(matches), `Unable to match '${suri}' to a secret URI`);
+  assert(!isNull(matches), 'Unable to match provided value to a secret URI');
 
   const [, phrase, , derivePath, , , password] = matches as string[];
   const { path } = keyExtractPath(derivePath);
