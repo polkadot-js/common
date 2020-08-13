0cee930a679e0b7c89f8039cc42af0fbd7d65c5f
Jaco Greeff <jacogr@gmail.com>
2020-08-13 15:30:30 +0200

    u8aToU8a

--- packages/util-crypto/src/secp256k1/hasher.ts
+++ packages/util-crypto/src/secp256k1/hasher.ts
@@ -4,16 +4,20 @@
 
 import { HashType } from './types';
 
+import { u8aToU8a } from '@polkadot/util';
+
 import { blake2AsU8a } from '../blake2';
 import { keccakAsU8a } from '../keccak';
 
 const HASH_TYPES = ['blake2', 'keccak'];
 
 export default function secp256k1Hasher (hashType: HashType, data: Uint8Array | string): Uint8Array {
+  const u8a = u8aToU8a(data);
+
   if (hashType === 'blake2') {
-    return blake2AsU8a(data);
+    return blake2AsU8a(u8a);
   } else if (hashType === 'keccak') {
-    return keccakAsU8a(data);
+    return keccakAsU8a(u8a);
   }
 
   throw new Error(`Unsupported secp256k1 hasher '${hashType as string}', expected one of ${HASH_TYPES.join(', ')}`);
