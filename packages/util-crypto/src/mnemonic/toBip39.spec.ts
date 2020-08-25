e5dd55e4194f15a17271c3097d92b31549e42fed
Jaco Greeff <jacogr@gmail.com>
2020-08-25 21:16:22 +0200

    Allow for WASM-as-available mnemonicToBip39 call (#669)

--- packages/util-crypto/src/mnemonic/toBip39.spec.ts
+++ packages/util-crypto/src/mnemonic/toBip39.spec.ts
@@ -3,11 +3,22 @@
 // of the Apache-2.0 license. See the LICENSE file for details.
 
 import { u8aToHex } from '@polkadot/util';
+import { waitReady } from '@polkadot/wasm-crypto';
 
 import { mnemonicToBip39 } from './';
 
 describe('mnemonicToBip39Seed', (): void => {
   it('generates a valid seed', (): void => {
+    expect(
+      u8aToHex(
+        mnemonicToBip39('seed sock milk update focus rotate barely fade car face mechanic mercy')
+      )
+    ).toEqual('0x3c121e20de068083b49c2315697fb59a2d9e8643c24e5ea7628132c58969a027');
+  });
+
+  it('generates the seed via WASM', async (): Promise<void> => {
+    await waitReady();
+
     expect(
       u8aToHex(
         mnemonicToBip39('seed sock milk update focus rotate barely fade car face mechanic mercy')
