dc0181b59298d71feeeda4a5cc4c11c23f11160b
Jaco Greeff <jacogr@gmail.com>
2020-09-20 09:30:08 +0200

    isBase64

--- packages/util-crypto/src/base64/validate.ts
+++ packages/util-crypto/src/base64/validate.ts
@@ -10,7 +10,7 @@ import { assert } from "@polkadot/util";
  * Validates the the supplied value is valid base64
  */
 export default function base64Validate (value?: string | null): true {
-  assert(value, `Empty base64 input`);
+  assert(value, 'Empty base64 input');
 
   if (!(/^(?:[A-Za-z0-9+/]{2}[A-Za-z0-9+/]{2})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/.test(value))) {
     throw new TypeError('Invalid base64 encoding');
