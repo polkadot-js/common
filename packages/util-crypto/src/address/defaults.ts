4cae42dbd1d943381f235527fad5c4fc8a57482d
Jaco Greeff <jacogr@gmail.com>
2020-10-16 12:00:16 +0200

    Expose all claimed/available networks

--- packages/util-crypto/src/address/defaults.ts
+++ packages/util-crypto/src/address/defaults.ts
@@ -1,13 +1,13 @@
 // Copyright 2017-2020 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { prefixes } from '@polkadot/networks';
+import { available } from '@polkadot/networks';
 
 const defaults = {
   allowedDecodedLengths: [1, 2, 4, 8, 32, 33],
   // publicKey has prefix + 2 checksum bytes, short only prefix + 1 checksum byte
   allowedEncodedLengths: [3, 4, 6, 10, 35, 36],
-  allowedPrefix: prefixes,
+  allowedPrefix: available.map(({ prefix }) => prefix),
   prefix: 42
 };
 
