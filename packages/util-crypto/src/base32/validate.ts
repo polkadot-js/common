3748ca02c6a2de355955f81b761c5dc09480a9cf
Jaco Greeff <jacogr@gmail.com>
2020-11-03 09:58:18 +0100

    startChar

--- packages/util-crypto/src/base32/validate.ts
+++ packages/util-crypto/src/base32/validate.ts
@@ -10,6 +10,6 @@ import { BASE32_ALPHABET } from './bs32';
  * @description
  * Validates the the supplied value is valid base32
  */
-export default function base32Validate (value?: string | null, ipfsCompat = false): true {
-  return validateChars('base32', BASE32_ALPHABET, value, ipfsCompat ? 'b' : undefined);
+export default function base32Validate (value?: string | null, ipfsCompat?: boolean): true {
+  return validateChars('base32', BASE32_ALPHABET, 'b', value, ipfsCompat);
 }
--- packages/util-crypto/src/base58/validate.ts
+++ packages/util-crypto/src/base58/validate.ts
@@ -5,11 +5,11 @@ import { assert } from '@polkadot/util';
 
 import { BASE58_ALPHABET } from './bs58';
 
-export function validateChars (type: string, alphabet: string, value?: string | null, startChar?: string): true {
+export function validateChars (type: string, alphabet: string, ipfsChar: string, value?: string | null, ipfsCompat = false): true {
   assert(value, `Expected non-null, non-empty ${type} input`);
-  assert(!startChar || value[0] === startChar, `Expected ${type} to start with '${startChar || ''}'`);
+  assert(!ipfsCompat || value[0] === ipfsChar, `Expected ${type} to start with '${ipfsChar}'`);
 
-  for (let i = (startChar ? 1 : 0); i < value.length; i++) {
+  for (let i = (ipfsCompat ? 1 : 0); i < value.length; i++) {
     assert(alphabet.includes(value[i]), `Invalid ${type} character "${value[i]}" (0x${value.charCodeAt(i).toString(16)}) at index ${i}`);
   }
 
@@ -22,6 +22,6 @@ export function validateChars (type: string, alphabet: string, value?: string | 
  * @description
  * Validates the the supplied value is valid base58
  */
-export default function base58Validate (value?: string | null, ipfsCompat = false): true {
-  return validateChars('base58', BASE58_ALPHABET, value, ipfsCompat ? 'z' : undefined);
+export default function base58Validate (value?: string | null, ipfsCompat?: boolean): true {
+  return validateChars('base58', BASE58_ALPHABET, 'z', value, ipfsCompat);
 }
