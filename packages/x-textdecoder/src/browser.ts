db0559e9c999c265ab033b3ec96fad5bc2b89d17
Jaco Greeff <jacogr@gmail.com>
2020-11-07 15:22:42 +0100

    RN-specific getRandomValues (no node crypto) (#747)
    * RN-specific getRandomValues (no node crypto)
    
    * Use polyfilled as available
    
    * === object

--- packages/x-textdecoder/src/browser.ts
+++ packages/x-textdecoder/src/browser.ts
@@ -1,17 +1,8 @@
 // Copyright 2017-2020 @polkadot/x-textencoder authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export default typeof TextDecoder === 'undefined'
-  ? class {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-useless-constructor
-    constructor (_: 'utf-8') {
-      // nothing
-    }
+import Fallback from './fallback';
 
-    decode (value: Uint8Array): string {
-      return value.reduce((str, code): string => {
-        return str + String.fromCharCode(code);
-      }, '');
-    }
-  }
+export default typeof TextDecoder === 'undefined'
+  ? Fallback
   : TextDecoder;
