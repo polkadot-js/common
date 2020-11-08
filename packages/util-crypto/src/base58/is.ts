49f6a43939dca7b4947e91ef11b53129a73b6663
Jaco Greeff <jacogr@gmail.com>
2020-11-08 10:53:50 +0100

    Dep-dup is functions

--- packages/util-crypto/src/base58/is.ts
+++ packages/util-crypto/src/base58/is.ts
@@ -1,12 +1,9 @@
 // Copyright 2017-2020 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { testValidator } from '../base32/is';
 import validate from './validate';
 
 export default function isBase58 (value?: string | null, ipfsCompat?: boolean): boolean {
-  try {
-    return validate(value, ipfsCompat);
-  } catch (error) {
-    return false;
-  }
+  return testValidator(validate, value, ipfsCompat);
 }
