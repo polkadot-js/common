90f9fb024091183edb8cb4cf0c4ea5766fcdfeb8
Jaco Greeff <jacogr@gmail.com>
2020-11-09 21:55:33 +0100

    Extract detectPackage /cryptoWait as side effect

---
+++ packages/keyring/src/detectPackage.ts
@@ -0,0 +1,7 @@
+// Copyright 2017-2020 @polkadot/keyring authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { detectPackage } from '@polkadot/util';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+detectPackage(require('./package.json'), typeof __dirname !== 'undefined' && __dirname);
--- packages/keyring/src/index.ts
+++ packages/keyring/src/index.ts
@@ -1,13 +1,11 @@
 // Copyright 2017-2020 @polkadot/keyring authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { detectPackage } from '@polkadot/util';
+import './detectPackage';
+
 import { decodeAddress, encodeAddress, setSS58Format } from '@polkadot/util-crypto';
 
 import { Keyring } from './keyring';
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-detectPackage(require('./package.json'), typeof __dirname !== 'undefined' && __dirname);
 
 export default Keyring;
 
