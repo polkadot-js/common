a58f9eab078a20a3831c753a33db67652cd5d857
Jaco Greeff <jacogr@gmail.com>
2020-12-13 12:25:22 +0100

    Allow for instanceId & normalize overrides

--- packages/util/src/memoize.ts
+++ packages/util/src/memoize.ts
@@ -7,23 +7,37 @@ import type { Memoized } from './types';
 
 import { isUndefined } from './is/undefined';
 
-export function memoize <T> (fn: (...args: any[]) => T): Memoized<T> {
-  const cache: Record<string, T> = {};
+interface Options {
+  getInstanceId?: () => string,
+  normalize?: (args: any[]) => string;
+}
+
+const INSTANCEID = () => 'none';
+const NORMALIZER = (args: any[]) => JSON.stringify({ args });
+
+export function memoize <T> (fn: (...args: any[]) => T, { getInstanceId = INSTANCEID, normalize = NORMALIZER }: Options = {}): Memoized<T> {
+  const cache: Record<string, Record<string, T>> = {};
 
   const memoized = (...args: any[]): T => {
-    const stringParams = JSON.stringify({ args });
+    const stringParams = normalize(args);
+    const instanceId = getInstanceId();
 
-    if (isUndefined(cache[stringParams])) {
-      cache[stringParams] = fn(...args);
+    if (!cache[instanceId]) {
+      cache[instanceId] = {};
     }
 
-    return cache[stringParams];
+    if (isUndefined(cache[instanceId][stringParams])) {
+      cache[instanceId][stringParams] = fn(...args);
+    }
+
+    return cache[instanceId][stringParams];
   };
 
   memoized.unmemoize = (...args: any[]): void => {
-    const stringParams = JSON.stringify({ args });
+    const stringParams = normalize(args);
+    const instanceId = getInstanceId();
 
-    if (!isUndefined(cache[stringParams])) {
+    if (cache[instanceId] && !isUndefined(cache[instanceId][stringParams])) {
       delete cache[stringParams];
     }
   };
