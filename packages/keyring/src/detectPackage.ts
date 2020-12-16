f569a34166ac32e4e9249ca636135a1c0140ce39
Jaco Greeff <jacogr@gmail.com>
2020-12-16 15:24:41 +0100

    Remove duplicate detectPackage

--- packages/keyring/src/detectPackage.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2020 @polkadot/keyring authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import { detectPackage } from '@polkadot/util';
-
-import packageInfo from './package-info.json';
-
-detectPackage(packageInfo, typeof __dirname !== 'undefined' && __dirname);
