31426858c690ea2ba3e05e0975268658a7d996e4
Jaco Greeff <jacogr@gmail.com>
2020-12-18 11:18:02 +0100

    detectPackage import revert

--- packages/util/src/bn/consts.ts
+++ packages/util/src/bn/consts.ts
@@ -7,36 +7,28 @@ import BN from 'bn.js';
  * @name BN_ZERO
  * @summary BN constant for 0.
  */
-const BN_ZERO: BN = new BN(0);
+export const BN_ZERO: BN = new BN(0);
 
 /**
  * @name BN_ONE
  * @summary BN constant for 1.
  */
-const BN_ONE: BN = new BN(1);
+export const BN_ONE: BN = new BN(1);
 
 /**
  * @name BN_TEN
  * @summary BN constant for 10.
  */
-const BN_TEN: BN = new BN(10);
+export const BN_TEN: BN = new BN(10);
 
 /**
  * @name BN_HUNDRED
  * @summary BN constant for 100.
  */
-const BN_HUNDRED: BN = new BN(100);
+export const BN_HUNDRED: BN = new BN(100);
 
 /**
  * @name BN_THOUSAND
  * @summary BN constant for 1000.
  */
-const BN_THOUSAND: BN = new BN(1000);
-
-export {
-  BN_ZERO,
-  BN_ONE,
-  BN_TEN,
-  BN_HUNDRED,
-  BN_THOUSAND
-};
+export const BN_THOUSAND: BN = new BN(1000);
