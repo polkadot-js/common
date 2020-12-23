8dad5537912c56468aab7b49c834fac4f5878f41
Jaco Greeff <jacogr@gmail.com>
2020-12-23 11:42:21 +0100

    Add test for arrayFlatten

---
+++ packages/util/src/array/flatten.spec.ts
@@ -0,0 +1,12 @@
+// Copyright 2017-2020 @polkadot/util authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { arrayFlatten } from '.';
+
+describe('arrayFlatten', (): void => {
+  it('flattens arrays', (): void => {
+    expect(
+      arrayFlatten([[0], [1, 2, 3], [4, 5], [6], [], [7, 8]])
+    ).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+});
