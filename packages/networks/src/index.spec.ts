436f6c2ec6f4ed9feed477b3c9c9c787d468c68b
Jaco Greeff <jacogr@gmail.com>
2020-12-27 12:38:16 +0100

    Filter ss58 testnets

--- packages/networks/src/index.spec.ts
+++ packages/networks/src/index.spec.ts
@@ -1,10 +1,14 @@
 // Copyright 2017-2020 @polkadot/networks authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import filtered from './';
+import filtered, { available } from '.';
 
 describe('filtered', (): void => {
   it('has the correct starting order', (): void => {
     expect(filtered.slice(0, 3).map(({ prefix }) => prefix)).toEqual([0, 2, 42]);
+  });
+
+  it('has no ignored networks', (): void => {
+    expect(available.some(({ isIgnored }) => isIgnored)).toEqual(false);
   });
 });
