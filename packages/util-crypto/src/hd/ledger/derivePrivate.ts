1b71ff001a66775512ba7c7ba8180bb84c0b634f
Jaco Greeff <jacogr@gmail.com>
2021-01-31 12:11:37 +0100

    Additional hdLedger tests

--- packages/util-crypto/src/hd/ledger/derivePrivate.ts
+++ packages/util-crypto/src/hd/ledger/derivePrivate.ts
@@ -1,34 +1,29 @@
 // Copyright 2017-2021 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import BN from 'bn.js';
-
-import { bnToU8a, u8aConcat } from '@polkadot/util';
+import { bnToU8a, u8aConcat, u8aToBn } from '@polkadot/util';
 
 import { hmacSha512 } from '../../hmac';
 
 // performs hard-only derivation on the xprv
 export function ledgerDerivePrivate (xprv: Uint8Array, index: number): Uint8Array {
-  const kl = xprv.slice(0, 32);
-  const kr = xprv.slice(32, 64);
-  const cc = xprv.slice(64, 96);
-
-  const data = new Uint8Array(1 + 64 + 4);
-
-  data.set(bnToU8a(index, { bitLength: 32, isLe: true }), 1 + 64);
-  data.set(kl, 1);
-  data.set(kr, 1 + 32);
-  data[0] = 0x00;
-
+  const kl = xprv.subarray(0, 32);
+  const kr = xprv.subarray(32, 64);
+  const cc = xprv.subarray(64, 96);
+  const data = u8aConcat([0], kl, kr, bnToU8a(index, { bitLength: 32, isLe: true }));
   const z = hmacSha512(cc, data);
 
   data[0] = 0x01;
 
-  const chainCode = hmacSha512(cc, data).slice(32, 64);
-  const zl = z.slice(0, 32);
-  const zr = z.slice(32, 64);
-  const left = bnToU8a(new BN(kl, 16, 'le').add(new BN(zl.slice(0, 28), 16, 'le').mul(new BN(8))), { bitLength: 512, isLe: true }).slice(0, 32);
-  const right = bnToU8a(new BN(kr, 16, 'le').add(new BN(zr, 16, 'le')), { bitLength: 512, isLe: true }).slice(0, 32);
-
-  return u8aConcat(left, right, chainCode);
+  return u8aConcat(
+    bnToU8a(
+      u8aToBn(kl, { isLe: true }).iadd(u8aToBn(z.subarray(0, 28), { isLe: true }).muln(8)),
+      { bitLength: 512, isLe: true }
+    ).subarray(0, 32),
+    bnToU8a(
+      u8aToBn(kr, { isLe: true }).iadd(u8aToBn(z.subarray(32, 64), { isLe: true })),
+      { bitLength: 512, isLe: true }
+    ).subarray(0, 32),
+    hmacSha512(cc, data).subarray(32, 64)
+  );
 }
