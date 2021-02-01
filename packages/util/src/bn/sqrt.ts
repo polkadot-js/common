5f82bff1c5a95fee1186528ba6920d7e0525166a
Jaco Greeff <jacogr@gmail.com>
2021-02-01 17:31:16 +0100

    inline

--- packages/util/src/bn/sqrt.ts
+++ packages/util/src/bn/sqrt.ts
@@ -8,7 +8,7 @@ import { BN_ONE } from '../bn';
 
 // https://golb.hplar.ch/2018/09/javascript-bigint.html
 function newtonIteration (n: BN, x0: BN): BN {
-  const x1 = n.div(x0).add(x0).shrn(1);
+  const x1 = n.div(x0).iadd(x0).ishrn(1);
 
   return (x0.eq(x1) || x0.eq(x1.sub(BN_ONE)))
     ? x0
