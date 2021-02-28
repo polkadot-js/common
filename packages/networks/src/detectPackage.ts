5133955efe528817fc1b87b8a506fee7cbd2f031
Jaco Greeff <jacogr@gmail.com>
2021-02-28 11:04:16 +0100

    5.9.2

--- packages/networks/src/detectPackage.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2017-2021 @polkadot/keyring authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import { detectPackage, packageInfo as utilInfo } from '@polkadot/util';
-import { packageInfo as fetchInfo } from '@polkadot/x-fetch';
-
-import { packageInfo } from './packageInfo';
-
-detectPackage(packageInfo, typeof __dirname !== 'undefined' && __dirname, [utilInfo, fetchInfo]);
