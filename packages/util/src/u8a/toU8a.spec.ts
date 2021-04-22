8a402c41f72a1a33faf4f6c795ba6cfac428600f
Jaco Greeff <jacogr@gmail.com>
2021-04-22 11:16:59 +0300

    Correct expectation

--- packages/util/src/u8a/toU8a.spec.ts
+++ packages/util/src/u8a/toU8a.spec.ts
@@ -59,7 +59,7 @@ describe('u8aToU8a', (): void => {
   it('fails to create from unknown typed values', (): void => {
     expect(
       // this is where completely invalid data is being passed
-      u8aToU8a(123 as unknown as Uint8Array)
-    ).toThrow(/Unable to convert 123 (typeof number) to a Uint8Array/);
+      () => u8aToU8a(123 as unknown as Uint8Array)
+    ).toThrow('Unable to convert 123 (typeof number) to a Uint8Array');
   });
 });
