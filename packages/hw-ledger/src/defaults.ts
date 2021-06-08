678e888a4a0156fb349e62f9de4252214c5c3937
Raymond Zhong <raykyri@users.noreply.github.com>
2021-06-08 02:17:39 -0700

    Add Edgeware ledger app (#1006)
    * Add Edgeware ledger app
    
    * Add edgeware slip44

--- packages/hw-ledger/src/defaults.ts
+++ packages/hw-ledger/src/defaults.ts
@@ -4,13 +4,14 @@
 import type Transport from '@ledgerhq/hw-transport';
 import type { SubstrateApp } from '@zondax/ledger-polkadot';
 
-import { newDockApp, newEquilibriumApp, newKusamaApp, newPolkadotApp, newPolymeshApp } from '@zondax/ledger-polkadot';
+import { newDockApp, newEdgewareApp, newEquilibriumApp, newKusamaApp, newPolkadotApp, newPolymeshApp } from '@zondax/ledger-polkadot';
 
 // These match up with the network keys in the @polkadot/networks package
 // (which is turn aligns with the substrate/ss58-registry.json as the single
 // source of truth)
 export const ledgerApps: Record<string, (transport: Transport) => SubstrateApp> = {
   'dock-mainnet': newDockApp,
+  edgeware: newEdgewareApp,
   equilibrium: newEquilibriumApp,
   kusama: newKusamaApp,
   polkadot: newPolkadotApp,
