c3777f72f2809c3b0fd54f4473be16b077998d3e
Keith Ingram <kwingram25@gmail.com>
2021-06-10 10:20:18 -1000

    Compute HDKD within function(s)

--- packages/util-crypto/src/nacl/deriveHard.ts
+++ packages/util-crypto/src/nacl/deriveHard.ts
@@ -5,9 +5,9 @@ import { compactAddLength, stringToU8a, u8aConcat } from '@polkadot/util';
 
 import { blake2AsU8a } from '../blake2/asU8a';
 
-const HDKD = compactAddLength(stringToU8a('Ed25519HDKD'));
-
 export function naclDeriveHard (seed: Uint8Array, chainCode: Uint8Array): Uint8Array {
+  const HDKD = compactAddLength(stringToU8a('Ed25519HDKD'));
+
   return blake2AsU8a(
     u8aConcat(HDKD, seed, chainCode)
   );
--- packages/util-crypto/src/secp256k1/deriveHard.ts
+++ packages/util-crypto/src/secp256k1/deriveHard.ts
@@ -5,9 +5,9 @@ import { compactAddLength, stringToU8a, u8aConcat } from '@polkadot/util';
 
 import { blake2AsU8a } from '../blake2/asU8a';
 
-const HDKD = compactAddLength(stringToU8a('Secp256k1HDKD'));
-
 export function secp256k1DeriveHard (seed: Uint8Array, chainCode: Uint8Array): Uint8Array {
+  const HDKD = compactAddLength(stringToU8a('Secp256k1HDKD'));
+
   // NOTE This is specific to the Substrate HDD derivation, so always use the blake2 hasher
   return blake2AsU8a(u8aConcat(HDKD, seed, chainCode), 256);
 }
