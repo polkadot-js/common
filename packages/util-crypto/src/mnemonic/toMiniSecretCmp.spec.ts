04169b2646ac203b3a6506509d18f1bda9c16784
Jaco Greeff <jacogr@gmail.com>
2021-06-18 13:09:09 +0300

    Prettify tests with .each

--- packages/util-crypto/src/mnemonic/toMiniSecretCmp.spec.ts
+++ packages/util-crypto/src/mnemonic/toMiniSecretCmp.spec.ts
@@ -5,52 +5,50 @@ import { arrayRange, u8aEq } from '@polkadot/util';
 
 import { cryptoWaitReady, mnemonicGenerate, mnemonicToMiniSecret, naclKeypairFromSeed, schnorrkelKeypairFromSeed } from '..';
 
-describe('mnemonicToMiniSecret (compare)', (): void => {
-  [true, false].forEach((onlyJsMnemonic): void => {
-    // generate either a JS or WASM mnemonic
-    describe(`onlyJs=${onlyJsMnemonic.toString()}`, (): void => {
-      // loop through lots of mnemonics
-      arrayRange(10_000).forEach((i): void => {
-        describe(`${i.toString().padStart(7, ' ')}`, (): void => {
-          let mnemonic: string;
-          let minisecret: Uint8Array;
-          let edpub: Uint8Array;
-          let srpub: Uint8Array;
+// NOTE: This basically controls how long stuff runs for, YMMV
+//
+// - 100 runs with 5 checks, takes 2mins on _my_ machine
+// - 10_000 runs with 5 checks should be ~3hrs
+const NUM_RUNS = 100;
+const NUM_CHECKS = 5;
 
-          beforeAll(async (): Promise<void> => {
-            await cryptoWaitReady();
+// generate either a JS or WASM mnemonic
+describe.each([true, false])('mnemonicToMiniSecret (compare), onlyJsMnemonic=%p', (onlyJsMnemonic): void => {
+  beforeAll(async (): Promise<void> => {
+    await cryptoWaitReady();
+  });
 
-            mnemonic = mnemonicGenerate(12, onlyJsMnemonic);
-            minisecret = mnemonicToMiniSecret(mnemonic, '', onlyJsMnemonic);
-            edpub = naclKeypairFromSeed(minisecret).publicKey;
-            srpub = schnorrkelKeypairFromSeed(minisecret).publicKey;
-          });
+  // loop through lots of mnemonics
+  describe.each(arrayRange(NUM_RUNS))('run=%p', (): void => {
+    // compare both JS and WASM outputs against original
+    describe.each([true, false])('onlyJsMini=%p', (onlyJsMini): void => {
+      // NOTE we cannot actually use the onlyJsMnemonic flag here
+      const mnemonic = mnemonicGenerate(12);
 
-          // do iterations to check and re-check that all matches
-          arrayRange(20).forEach((i): void => {
-            it(`${i.toString().padStart(3, ' ')}: ${mnemonic}`, (): void => {
-              // compare both JS and WASM outputs against original
-              [true, false].forEach((onlyJsMini): void => {
-                const testmini = mnemonicToMiniSecret(mnemonic, '', onlyJsMini);
+      describe(mnemonic, (): void => {
+        // do iterations to check and re-check that all matches
+        it.concurrent.each(arrayRange(NUM_CHECKS))('check=%p', (): void => {
+          const minisecret = mnemonicToMiniSecret(mnemonic, '', onlyJsMnemonic);
+          const edpub = naclKeypairFromSeed(minisecret).publicKey;
+          const srpub = schnorrkelKeypairFromSeed(minisecret).publicKey;
 
-                // explicit minisecret compare
-                expect(
-                  u8aEq(minisecret, testmini)
-                ).toEqual(true);
+          const testmini = mnemonicToMiniSecret(mnemonic, '', onlyJsMini);
 
-                // compare the sr25519 keypair generated
-                expect(
-                  u8aEq(srpub, schnorrkelKeypairFromSeed(testmini).publicKey)
-                ).toEqual(true);
+          // explicit minisecret compare
+          expect(
+            u8aEq(minisecret, testmini)
+          ).toEqual(true);
 
-                // compare ed both in WASM and JS
-                [true, false].forEach((onlyJsEd): void => {
-                  expect(
-                    u8aEq(edpub, naclKeypairFromSeed(testmini, onlyJsEd).publicKey)
-                  ).toEqual(true);
-                });
-              });
-            });
+          // compare the sr25519 keypair generated
+          expect(
+            u8aEq(srpub, schnorrkelKeypairFromSeed(testmini).publicKey)
+          ).toEqual(true);
+
+          // compare ed both in WASM and JS
+          [true, false].forEach((onlyJsEd): void => {
+            expect(
+              u8aEq(edpub, naclKeypairFromSeed(testmini, onlyJsEd).publicKey)
+            ).toEqual(true);
           });
         });
       });
