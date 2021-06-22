4a2cc505ff35df18db17846e3b70508394c7e031
Jaco Greeff <jacogr@gmail.com>
2021-06-22 16:37:49 +0300

    Bump deps

--- packages/hw-ledger-transports/src/ledgerhq-web.d.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2021 @polkadot/hw-ledger authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-declare module '@ledgerhq/hw-transport-webhid' {
-  import Transport from '@ledgerhq/hw-transport';
-
-  export default class TransportWebHid extends Transport {}
-}
