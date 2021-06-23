57b57192dd3ff9d2ba4c22ffe8c0847c629d9354
estienne.antoine@gmail.com <estienne.antoine@gmail.com>
2021-06-23 15:42:23 +0200

    exception for 20 address

--- packages/keyring/src/pair/index.ts
+++ packages/keyring/src/pair/index.ts
@@ -176,8 +176,10 @@ export function createPair ({ toSS58, type }: Setup, { publicKey, secretKey }: P
     toJson: (passphrase?: string): KeyringPair$Json => {
       // NOTE: For ecdsa and ethereum, the publicKey cannot be extracted from the address. For these
       // pass the hex-encoded publicKey through to the address portion of the JSON (before decoding)
+      // unless the publicKey is already an address
+      console.log("publicKey.length",publicKey.length)
       const address = ['ecdsa', 'ethereum'].includes(type)
-        ? u8aToHex(secp256k1Compress(publicKey))
+        ? publicKey.length===21?u8aToHex(secp256k1Compress(publicKey)):u8aToHex(publicKey)
         : encodeAddress();
 
       return pairToJson(type, { address, meta }, recode(passphrase), !!passphrase);
