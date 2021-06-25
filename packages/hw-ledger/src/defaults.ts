fbe688ff4e3dbb549b2bc07aa8309a0325366d43
Jaco <jacogr@gmail.com>
2021-06-25 15:40:06 +0300

    Ledger info for Statemine (#1026)

--- packages/hw-ledger/src/defaults.ts
+++ packages/hw-ledger/src/defaults.ts
@@ -4,16 +4,15 @@
 import type Transport from '@ledgerhq/hw-transport';
 import type { SubstrateApp } from '@zondax/ledger-substrate';
 
-import { newDockApp, newEdgewareApp, newEquilibriumApp, newKusamaApp, newPolkadotApp, newPolymeshApp } from '@zondax/ledger-substrate';
+import { newDockApp, newEdgewareApp, newEquilibriumApp, newKusamaApp, newPolkadotApp, newPolymeshApp, newStateMineApp } from '@zondax/ledger-substrate';
 
-// These match up with the network keys in the @polkadot/networks package
-// (which is turn aligns with the substrate/ss58-registry.json as the single
-// source of truth)
+// These match up with the keys of the knownLedger object in the @polkadot/networks/defaults.ts
 export const ledgerApps: Record<string, (transport: Transport) => SubstrateApp> = {
   'dock-mainnet': newDockApp,
   edgeware: newEdgewareApp,
   equilibrium: newEquilibriumApp,
   kusama: newKusamaApp,
   polkadot: newPolkadotApp,
-  polymesh: newPolymeshApp
+  polymesh: newPolymeshApp,
+  statemine: newStateMineApp
 };
