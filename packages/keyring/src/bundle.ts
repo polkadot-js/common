55abbd50065a91d33593a5edc140ca70bf6cfc81
Jaco Greeff <jacogr@gmail.com>
2021-07-06 10:20:37 +0300

    Add bundle entrypoint

---
+++ packages/keyring/src/bundle.ts
@@ -0,0 +1,10 @@
+// Copyright 2017-2021 @polkadot/keyring authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { Keyring } from './keyring';
+
+export { decodeAddress, encodeAddress, setSS58Format } from '@polkadot/util-crypto';
+
+export { packageInfo } from './packageInfo';
+
+export { Keyring };
