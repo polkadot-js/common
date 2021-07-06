a461c19ddc880d1b06dccec45a588a19c8f7d018
Jaco <jacogr@gmail.com>
2021-07-06 08:03:49 +0300

    Apply suggestions from code review

--- packages/util-crypto/src/detectPackage.ts
+++ packages/util-crypto/src/detectPackage.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2021 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { packageInfo as netInfo } from '@polkadot/networks';
+import { packageInfo as netInfo } from '@polkadot/networks/packageInfo';
 import { detectPackage, packageInfo as utilInfo } from '@polkadot/util';
 import { packageInfo as randomInfo } from '@polkadot/x-randomvalues';
 
