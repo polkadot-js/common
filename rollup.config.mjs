2aa3663e288cfec5c5a962ed03700d3c97519176
Jaco Greeff <jacogr@gmail.com>
2021-07-06 08:35:24 +0300

    Use createBundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,92 +1,51 @@
 // Copyright 2019-2021 @polkadot/wasm-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { createInput, createOutput, createPlugins } from '@polkadot/dev/config/rollup';
+import { createBundle } from '@polkadot/dev/config/rollup';
 
-const external = [
+const pkgs = [
+  '@polkadot/hw-ledger',
+  '@polkadot/keyring',
   '@polkadot/networks',
   '@polkadot/util',
   '@polkadot/util-crypto',
-  '@polkadot/wasm-crypto',
+  '@polkadot/x-fetch', // browser
   '@polkadot/x-global',
-  '@polkadot/x-randomvalues',
-  '@polkadot/x-textdecoder',
-  '@polkadot/x-textencoder'
+  '@polkadot/x-randomvalues', // browser
+  '@polkadot/x-rxjs',
+  '@polkadot/x-textdecoder', // browser
+  '@polkadot/x-textencoder', // browser
+  '@polkadot/x-ws'
 ];
 
-export default [
-  {
-    external,
-    input: createInput('hw-ledger'),
-    output: createOutput('hw-ledger', external),
-    plugins: createPlugins([
-      { find: '@polkadot/hw-ledger-transports', replacement: '../../hw-ledger-transports/build' }
-    ])
-  },
-  {
-    external,
-    input: createInput('keyring'),
-    output: createOutput('keyring', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('networks'),
-    output: createOutput('networks', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('util'),
-    output: createOutput('util', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('util-crypto'),
-    output: createOutput('util-crypto', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('x-fetch', 'browser.js'),
-    output: createOutput('x-fetch', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('x-global'),
-    output: createOutput('x-global', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('x-randomvalues', 'browser.js'),
-    output: createOutput('x-randomvalues', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('x-rxjs'),
-    output: createOutput('x-rxjs', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('x-textdecoder', 'browser.js'),
-    output: createOutput('x-textdecoder', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('x-textencoder', 'browser.js'),
-    output: createOutput('x-textencoder', external),
-    plugins: createPlugins()
-  },
-  {
-    external,
-    input: createInput('x-ws', 'browser.js'),
-    output: createOutput('x-ws', external),
-    plugins: createPlugins()
-  }
+const external = [
+  ...pkgs,
+  '@polkadot/wasm-crypto'
 ];
+
+const overrides = {
+  '@polkadot/hwledger': {
+    entries: [
+      { find: '@polkadot/hw-ledger-transports', replacement: '../../hw-ledger-transports/build' }
+    ]
+  },
+  '@polkadot/x-fetch': {
+    index: 'browser.js'
+  },
+  '@polkadot/x-randomvalues': {
+    index: 'browser.js'
+  },
+  '@polkadot/x-textdecoder': {
+    index: 'browser.js'
+  },
+  '@polkadot/x-textencoder': {
+    index: 'browser.js'
+  },
+  '@polkadot/x-ws': {
+    index: 'browser.js'
+  }
+};
+
+export default pkgs.map((pkg) =>
+  createBundle({ ...(overrides[pkg] || {}), external, pkg })
+);
