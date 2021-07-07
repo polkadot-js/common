6ea1ee555b02be83ead874bd9c2a965895b57e09
Jaco <jacogr@gmail.com>
2021-07-07 10:25:08 +0300

    Remove the x-rxjs compatibility layer (#1050)
    * Remove the x-rxjs compatibility layer
    
    * Update CHANGELOG.md

--- packages/x-rxjs/src/operators.ts
+++
@@ -1,5 +0,0 @@
-// Copyright 2017-2021 @polkadot/x-rxjs authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-// this is deprecated, only provided here for backwards compatibility
-export * from 'rxjs/operators';
