ae60b501f8119e757653312922f685f349e7a073
Jaco Greeff <jacogr@gmail.com>
2021-08-26 09:15:39 +0300

    schnorrkelAgreement

--- packages/util-crypto/src/schnorrkel/keypair/fromSeed.ts
+++ packages/util-crypto/src/schnorrkel/keypair/fromSeed.ts
@@ -3,6 +3,7 @@
 
 import type { Keypair } from '../../types';
 
+import { assert, u8aToU8a } from '@polkadot/util';
 import { sr25519KeypairFromSeed } from '@polkadot/wasm-crypto';
 
 import { schnorrkelKeypairFromU8a } from './fromU8a';
@@ -11,8 +12,12 @@ import { schnorrkelKeypairFromU8a } from './fromU8a';
  * @name schnorrkelKeypairFromSeed
  * @description Returns a object containing a `publicKey` & `secretKey` generated from the supplied seed.
  */
-export function schnorrkelKeypairFromSeed (seed: Uint8Array): Keypair {
+export function schnorrkelKeypairFromSeed (seed: Uint8Array | string): Keypair {
+  const seedU8a = u8aToU8a(seed);
+
+  assert(seedU8a.length === 32, () => `Expected a seed matching 32 bytes, found ${seedU8a.length}`);
+
   return schnorrkelKeypairFromU8a(
-    sr25519KeypairFromSeed(seed)
+    sr25519KeypairFromSeed(seedU8a)
   );
 }
