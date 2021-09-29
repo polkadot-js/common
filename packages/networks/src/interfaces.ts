c8dfeaad5b9cf9c982a746a32d7ee860d8cca17f
Jaco <jacogr@gmail.com>
2021-09-29 15:51:19 +0300

    Update ss58 JSON format handling (#1120)
    * Update JSON handling
    
    * Fix output

--- packages/networks/src/interfaces.ts
+++ packages/networks/src/interfaces.ts
@@ -18,7 +18,7 @@ export const allNetworks = knownSubstrate.map((o): SubstrateNetwork => {
 
   n.genesisHash = knownGenesis[network] || [];
   n.icon = knownIcon[network] || 'substrate';
-  n.isIgnored = !network || !!knownTestnet[network] || network.startsWith('reserved');
+  n.isIgnored = !network || !!knownTestnet[network] || !o.standardAccount;
 
   return n;
 });
