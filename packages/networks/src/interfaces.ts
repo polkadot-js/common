3c1aac44dd283d2128825b94efb73bb3b68cc920
Jaco <jacogr@gmail.com>
2021-10-23 12:28:22 +0300

    prefix 42

--- packages/networks/src/interfaces.ts
+++ packages/networks/src/interfaces.ts
@@ -18,7 +18,7 @@ export const allNetworks = knownSubstrate.map((o): SubstrateNetwork => {
 
   n.genesisHash = knownGenesis[network] || [];
   n.icon = knownIcon[network] || 'substrate';
-  n.isIgnored = !!knownTestnet[network] || !(o.standardAccount && o.decimals && o.symbols);
+  n.isIgnored = !!knownTestnet[network] || (!(o.standardAccount && o.decimals && o.symbols) && o.prefix !== 42);
 
   return n;
 });
