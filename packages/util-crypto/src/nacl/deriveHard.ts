9eabf95fc0be6df4a7fca2e03c27465795f3c90a
Jaco <jacogr@gmail.com>
2021-10-25 07:04:37 +0200

    Assert chainCode inputs to derive* (#1142)
    * Assert chainCode inputs to derive*
    
    * Adjust

--- packages/util-crypto/src/nacl/deriveHard.ts
+++ packages/util-crypto/src/nacl/deriveHard.ts
@@ -1,13 +1,15 @@
 // Copyright 2017-2021 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { compactAddLength, stringToU8a, u8aConcat } from '@polkadot/util';
+import { assert, compactAddLength, isU8a, stringToU8a, u8aConcat } from '@polkadot/util';
 
 import { blake2AsU8a } from '../blake2/asU8a';
 
 const HDKD = compactAddLength(stringToU8a('Ed25519HDKD'));
 
 export function naclDeriveHard (seed: Uint8Array, chainCode: Uint8Array): Uint8Array {
+  assert(isU8a(chainCode) && chainCode.length === 32, 'Invalid chainCode passed to derive');
+
   return blake2AsU8a(
     u8aConcat(HDKD, seed, chainCode)
   );
--- packages/util-crypto/src/schnorrkel/derive.ts
+++ packages/util-crypto/src/schnorrkel/derive.ts
@@ -1,20 +1,19 @@
 // Copyright 2017-2021 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import type { HexString } from '@polkadot/util/types';
 import type { Keypair } from '../types';
 
-import { u8aToU8a } from '@polkadot/util';
+import { assert, isU8a } from '@polkadot/util';
 
 import { schnorrkelKeypairFromU8a } from './keypair/fromU8a';
 import { schnorrkelKeypairToU8a } from './keypair/toU8a';
 
-export function createDeriveFn (derive: (pair: Uint8Array, cc: Uint8Array) => Uint8Array): (keypair: Keypair, chainCode: HexString | Uint8Array | string) => Keypair {
-  return (keypair: Keypair, chainCode: HexString | Uint8Array | string) =>
-    schnorrkelKeypairFromU8a(
-      derive(
-        schnorrkelKeypairToU8a(keypair),
-        u8aToU8a(chainCode)
-      )
+export function createDeriveFn (derive: (pair: Uint8Array, cc: Uint8Array) => Uint8Array): (keypair: Keypair, chainCode: Uint8Array) => Keypair {
+  return (keypair: Keypair, chainCode: Uint8Array): Keypair => {
+    assert(isU8a(chainCode) && chainCode.length === 32, 'Invalid chainCode passed to derive');
+
+    return schnorrkelKeypairFromU8a(
+      derive(schnorrkelKeypairToU8a(keypair), chainCode)
     );
+  };
 }
