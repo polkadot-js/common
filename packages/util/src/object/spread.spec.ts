6c69b8d96d4a13d25438cc892ca62cd3a6d22c17
Jaco <jacogr@gmail.com>
2021-11-04 08:07:18 +0200

    Allow null/undefined as a source

--- packages/util/src/object/spread.spec.ts
+++ packages/util/src/object/spread.spec.ts
@@ -9,4 +9,10 @@ describe('objectSpread', (): void => {
       objectSpread({ a: 1 }, { b: 2 }, { a: 2, c: { d: 3, e: 4 } })
     ).toEqual({ a: 2, b: 2, c: { d: 3, e: 4 } });
   });
+
+  it('spreads an object from multiple sources (with null & undefined)', (): void => {
+    expect(
+      objectSpread({ a: 1 }, null, { b: 2 }, undefined, { a: 2, c: { d: 3, e: 4 } })
+    ).toEqual({ a: 2, b: 2, c: { d: 3, e: 4 } });
+  });
 });
