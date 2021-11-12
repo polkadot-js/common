5b8f9cde56ef45bfa640950192390e6cf5701b50
Jaco <jacogr@gmail.com>
2021-11-12 09:15:03 +0200

    u8aToHex flatten

--- packages/util/src/u8a/toHex.ts
+++ packages/util/src/u8a/toHex.ts
@@ -10,7 +10,7 @@ type HexByte = `${HexDigit}${HexDigit}`;
 const ALPHABET = arrayRange(256).map((n) => n.toString(16).padStart(2, '0') as HexByte);
 
 /** @internal */
-function extract (value: Uint8Array): string {
+function hex (value: Uint8Array): string {
   const result = new Array<HexByte>(value.length);
 
   for (let i = 0; i < value.length; i++) {
@@ -18,20 +18,6 @@ function extract (value: Uint8Array): string {
   }
 
   return result.join('');
-}
-
-/** @internal */
-function unprefixed (value: Uint8Array, bitLength = -1): string {
-  const byteLength = Math.ceil(bitLength / 8);
-
-  return (byteLength > 0 && value.length > byteLength)
-    ? trim(value, Math.ceil(byteLength / 2))
-    : extract(value);
-}
-
-/** @internal */
-function trim (value: Uint8Array, halfLength: number): string {
-  return `${unprefixed(value.subarray(0, halfLength))}…${unprefixed(value.subarray(value.length - halfLength))}`;
 }
 
 /**
@@ -49,9 +35,13 @@ function trim (value: Uint8Array, halfLength: number): string {
  * ```
  */
 export function u8aToHex (value?: Uint8Array | null, bitLength = -1, isPrefixed = true): HexString {
+  const length = Math.ceil(bitLength / 8);
+
   return `${isPrefixed ? '0x' : ''}${
     !value || !value.length
       ? ''
-      : unprefixed(value, bitLength)
+      : (length > 0 && value.length > length)
+        ? `${hex(value.subarray(0, length / 2))}…${hex(value.subarray(value.length - length / 2))}`
+        : hex(value)
   }` as HexString;
 }
