d9231d26aa61caebace0734e04b74891f538b007
Jaco <jacogr@gmail.com>
2021-11-13 10:38:07 +0100

    Additional Node-specific u8a utils (#1177)
    * Additional Node-specific u8a utils
    
    * toHexBuffer

---
+++ packages/util/src/u8a/concatBuffer.spec.ts
@@ -0,0 +1,30 @@
+// Copyright 2017-2021 @polkadot/util authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { u8aConcat } from './concatBuffer';
+
+describe('u8aConcat', (): void => {
+  it('concatenates arrays', (): void => {
+    expect(
+      u8aConcat(
+        new Uint8Array([1, 2, 3, 4]),
+        new Uint8Array([5, 6]),
+        new Uint8Array([7, 8, 9])
+      )
+    ).toEqual(
+      new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    );
+  });
+
+  it('concatenates arrays & hex values', (): void => {
+    expect(
+      u8aConcat(
+        new Uint8Array([1, 2, 3, 4]),
+        '0x0506',
+        '0x070809'
+      )
+    ).toEqual(
+      new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    );
+  });
+});
