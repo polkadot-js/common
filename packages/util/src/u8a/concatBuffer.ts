83270d8e95e3983b6a6e0f83b3fca1a5b5d2ca94
Jaco <jacogr@gmail.com>
2021-11-13 09:56:49 +0200

    Additional Node-specific u8a utils

---
+++ packages/util/src/u8a/concatBuffer.ts
@@ -0,0 +1,33 @@
+// Copyright 2017-2021 @polkadot/util authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { U8aLike } from '../types';
+
+import { u8aToU8a } from './toU8a';
+
+/**
+ * @name u8aConcat
+ * @summary Creates a concatenated Uint8Array from the inputs.
+ * @description
+ * Concatenates the input arrays into a single `UInt8Array`.
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * import { { u8aConcat } from '@polkadot/util';
+ *
+ * u8aConcat(
+ *   new Uint8Array([1, 2, 3]),
+ *   new Uint8Array([4, 5, 6])
+ * ); // [1, 2, 3, 4, 5, 6]
+ * ```
+ */
+export function u8aConcat (...list: U8aLike[]): Uint8Array {
+  const u8as = new Array<Uint8Array>(list.length);
+
+  for (let i = 0; i < list.length; i++) {
+    u8as[i] = u8aToU8a(list[i]);
+  }
+
+  return Uint8Array.from(Buffer.concat(u8as));
+}
