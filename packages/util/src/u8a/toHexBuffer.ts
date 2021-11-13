c6d9ac342a6f2b1e3c6bbdb5af4f6e800114dc9d
Jaco <jacogr@gmail.com>
2021-11-13 11:31:12 +0200

    toHexBuffer

---
+++ packages/util/src/u8a/toHexBuffer.ts
@@ -0,0 +1,30 @@
+// Copyright 2017-2021 @polkadot/util authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { HexString } from '../types';
+
+/**
+ * @name u8aToHex
+ * @summary Creates a hex string from a Uint8Array object.
+ * @description
+ * `UInt8Array` input values return the actual hex string. `null` or `undefined` values returns an `0x` string.
+ * @example
+ * <BR>
+ *
+ * ```javascript
+ * import { u8aToHex } from '@polkadot/util';
+ *
+ * u8aToHex(new Uint8Array([0x68, 0x65, 0x6c, 0x6c, 0xf])); // 0x68656c0f
+ * ```
+ */
+export function u8aToHex (value?: Uint8Array | null, bitLength = -1, isPrefixed = true): HexString {
+  const length = Math.ceil(bitLength / 8);
+
+  return `${isPrefixed ? '0x' : ''}${
+    !value || !value.length
+      ? ''
+      : (length > 0 && value.length > length)
+        ? `${Buffer.from(value.subarray(0, length / 2)).toString('hex')}…${Buffer.from(value.subarray(value.length - length / 2)).toString('hex')}`
+        : Buffer.from(value).toString('hex')
+  }` as HexString;
+}
