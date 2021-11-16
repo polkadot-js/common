8deeba347b6b8c8dabfca1690700b92f46494573
Jaco <jacogr@gmail.com>
2021-11-16 16:25:05 +0200

    Move type def to types

--- packages/util/src/bi/class.ts
+++ packages/util/src/bi/class.ts
@@ -1,26 +1,9 @@
 // Copyright 2017-2021 @polkadot/util authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import type { IBigIntConstructor } from '../types';
+
 // See https://stackoverflow.com/questions/58207487/extend-object-wrappers-for-modern-primitives
-
-interface IBigIntConstructor {
-  new (value: string | number | bigint | boolean): bigint;
-
-  /**
-  * Interprets the low bits of a BigInt as a 2's-complement signed integer.
-  * All higher bits are discarded.
-  * @param bits The number of low bits to use
-  * @param int The BigInt whose bits to extract
-  */
-  asIntN (bits: number, int: bigint): bigint;
-  /**
-  * Interprets the low bits of a BigInt as an unsigned integer.
-  * All higher bits are discarded.
-  * @param bits The number of low bits to use
-  * @param int The BigInt whose bits to extract
-  */
-  asUintN (bits: number, int: bigint): bigint;
-}
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
--- packages/util/src/types.ts
+++ packages/util/src/types.ts
@@ -65,3 +65,23 @@ export type HexDigit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 export type HexString = `0x${string}`;
 
 export type U8aLike = HexString | number[] | Buffer | Uint8Array | AnyString;
+
+export interface IBigIntConstructor {
+  new (value: string | number | bigint | boolean): bigint;
+
+  /**
+  * Interprets the low bits of a BigInt as a 2's-complement signed integer.
+  * All higher bits are discarded.
+  * @param bits The number of low bits to use
+  * @param int The BigInt whose bits to extract
+  */
+  asIntN (bits: number, int: bigint): bigint;
+
+  /**
+  * Interprets the low bits of a BigInt as an unsigned integer.
+  * All higher bits are discarded.
+  * @param bits The number of low bits to use
+  * @param int The BigInt whose bits to extract
+  */
+  asUintN (bits: number, int: bigint): bigint;
+}
