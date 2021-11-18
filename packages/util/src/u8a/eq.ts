2f3e51c324ad522bd9aaa9455da0e85f8f1aa208
Jaco <jacogr@gmail.com>
2021-11-18 08:33:35 +0200

    Flatten calls

--- packages/util/src/u8a/eq.ts
+++ packages/util/src/u8a/eq.ts
@@ -5,31 +5,6 @@ import type { HexString } from '../types';
 
 import { u8aToU8a } from './toU8a';
 
-function equals (a: Uint8Array, b: Uint8Array): boolean {
-  if (a.length === b.length) {
-    const dvA = new DataView(a.buffer, a.byteOffset);
-    const dvB = new DataView(b.buffer, b.byteOffset);
-    const mod = a.length % 4;
-    const length = a.length - mod;
-
-    for (let i = 0; i < length; i += 4) {
-      if (dvA.getUint32(i) !== dvB.getUint32(i)) {
-        return false;
-      }
-    }
-
-    for (let i = length; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  return false;
-}
-
 /**
  * @name u8aEq
  * @summary Compares two Uint8Arrays for equality.
@@ -45,5 +20,29 @@ function equals (a: Uint8Array, b: Uint8Array): boolean {
  * ```
  */
 export function u8aEq (a: HexString | Uint8Array | string, b: HexString | Uint8Array | string): boolean {
-  return equals(u8aToU8a(a), u8aToU8a(b));
+  const u8aa = u8aToU8a(a);
+  const u8ab = u8aToU8a(b);
+
+  if (u8aa.length === u8ab.length) {
+    const dvA = new DataView(u8aa.buffer, u8aa.byteOffset);
+    const dvB = new DataView(u8ab.buffer, u8ab.byteOffset);
+    const mod = u8aa.length % 4;
+    const length = u8aa.length - mod;
+
+    for (let i = 0; i < length; i += 4) {
+      if (dvA.getUint32(i) !== dvB.getUint32(i)) {
+        return false;
+      }
+    }
+
+    for (let i = length; i < u8aa.length; i++) {
+      if (u8aa[i] !== u8ab[i]) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  return false;
 }
