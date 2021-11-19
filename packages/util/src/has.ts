4475e7c9170f314a62adf8fec4c47a15b37ad6c5
Jaco <jacogr@gmail.com>
2021-11-19 09:23:57 +0200

    hasWasm

--- packages/util/src/has.ts
+++ packages/util/src/has.ts
@@ -5,3 +5,4 @@ export const hasBigInt = typeof BigInt !== 'undefined';
 export const hasBuffer = typeof Buffer !== 'undefined';
 export const hasDirname = typeof __dirname !== 'undefined';
 export const hasProcess = typeof process === 'object';
+export const hasWasm = typeof WebAssembly !== 'undefined';
