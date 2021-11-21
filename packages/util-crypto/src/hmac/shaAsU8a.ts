5e9250eecc1a65f990b6c6eb4520f4f6b4983932
Jaco <jacogr@gmail.com>
2021-11-21 08:45:11 +0200

    Re-add helpers

--- packages/util-crypto/src/hmac/shaAsU8a.ts
+++ packages/util-crypto/src/hmac/shaAsU8a.ts
@@ -6,6 +6,13 @@ import hash from 'hash.js';
 import { u8aToU8a } from '@polkadot/util';
 import { hmacSha256, hmacSha512, isReady } from '@polkadot/wasm-crypto';
 
+type HashFn = (key: Uint8Array | string, data: Uint8Array, onlyJs?: boolean) => Uint8Array;
+
+function createSha (bitLength: 256 | 512 = 256): HashFn {
+  return (key: Uint8Array | string, data: Uint8Array, onlyJs?: boolean): Uint8Array =>
+    hmacShaAsU8a(key, data, bitLength, onlyJs);
+}
+
 export function hmacShaAsU8a (key: Uint8Array | string, data: Uint8Array, bitLength: 256 | 512 = 256, onlyJs = false): Uint8Array {
   const is256 = bitLength === 256;
   const u8aKey = u8aToU8a(key);
@@ -24,3 +31,6 @@ export function hmacShaAsU8a (key: Uint8Array | string, data: Uint8Array, bitLen
         : hash.hmac(hash.sha512, u8aKey).update(data).digest()
     );
 }
+
+export const hmacSha256AsU8a = createSha(256);
+export const hmacSha512AsU8a = createSha(512);
