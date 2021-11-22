5af0ab9c53118e126c5d8c0d5384d273ff98b25c
Jaco <jacogr@gmail.com>
2021-11-22 15:39:44 +0200

    readability

--- packages/util/src/string/camelCase.ts
+++ packages/util/src/string/camelCase.ts
@@ -3,21 +3,25 @@
 
 import type { AnyString } from '../types';
 
+const RE_WHITE = /[-_ ]+/g;
+const RE_WORDS = /(?:^\w|[A-Z]|\b\w)/g;
+const RE_JOIN = /\s/g;
+
 // Adapted from https://stackoverflow.com/a/2970667
 //
 // this is not as optimal as the original (we split into multiple), however
-// it does pass the tests (which the original doesn't) and it is still 10x
+// it does pass the tests (which the original doesn't) and it is still 10+x
 // improvement over the original camelcase npm package (at running)
 //
 // original: 20.88 μs/op
-//     this:  2.06 μs/op
+//     this:  1.70 μs/op
 function converter (replacer: (w: string, i: number) => string): (value: AnyString) => string {
   return (value: AnyString): string =>
     value
       .toString()
-      .replace(/[-_ ]+/g, ' ')
-      .replace(/(?:^\w|[A-Z]|\b\w)/g, replacer)
-      .replace(/\s/g, '');
+      .replace(RE_WHITE, ' ')
+      .replace(RE_WORDS, replacer)
+      .replace(RE_JOIN, '');
 }
 
 /**
