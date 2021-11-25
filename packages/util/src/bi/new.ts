59f2d139e40cff1efd62e46c55b39c910da92f83
Jaco <jacogr@gmail.com>
2021-11-25 10:45:51 +0200

    comments

--- packages/util/src/bi/new.ts
+++ packages/util/src/bi/new.ts
@@ -3,6 +3,10 @@
 
 import { hasBigInt } from '../has';
 
+/**
+ * @name newBigInt
+ * @description Creates a new instance of BigInt in environments that do support it
+ */
 export function newBigInt (n: bigint | number | string): bigint {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
