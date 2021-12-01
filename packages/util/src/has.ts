e33ee25efa04fd335aa62ee4414b00867cce5427
Jaco <jacogr@gmail.com>
2021-12-01 21:45:49 +0200

    x-bigint in has*

--- packages/util/src/has.ts
+++ packages/util/src/has.ts
@@ -1,9 +1,9 @@
 // Copyright 2017-2021 @polkadot/util authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { xglobal } from '@polkadot/x-global';
+import { BigInt } from '@polkadot/x-bigint';
 
-export const hasBigInt = typeof xglobal.BigInt === 'function' && typeof xglobal.BigInt.asIntN === 'function';
+export const hasBigInt = typeof BigInt === 'function' && typeof BigInt.asIntN === 'function';
 export const hasBuffer = typeof Buffer !== 'undefined';
 export const hasDirname = typeof __dirname !== 'undefined';
 export const hasProcess = typeof process === 'object';
