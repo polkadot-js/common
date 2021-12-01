288e458df7171ef07e04b7960c0d1d737ae75779
Jaco <jacogr@gmail.com>
2021-12-01 18:05:23 +0200

    Expand has checks

--- packages/util-crypto/src/cryptoInit.ts
+++ packages/util-crypto/src/cryptoInit.ts
@@ -1,7 +1,14 @@
 // Copyright 2017-2021 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+// import { xglobal } from '@polkadot/x-global';
+
 import { cryptoWaitReady } from './crypto';
+
+// This would not be a great idea since it would poluse the global namespace
+// if (typeof BigInt !== 'function' || typeof BigInt.asIntN !== 'function') {
+//   (xglobal as Record<string, unknown>).BigInt = () => Number.NaN;
+// }
 
 // start init process immediately
 cryptoWaitReady().catch((): void => {
