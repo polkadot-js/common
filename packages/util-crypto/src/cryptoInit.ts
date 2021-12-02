231353a095271f0cab64def69ba5f893b5e5a564
Jaco <jacogr@gmail.com>
2021-12-02 08:50:36 +0200

    Cleanup cryptoInit

--- packages/util-crypto/src/cryptoInit.ts
+++ packages/util-crypto/src/cryptoInit.ts
@@ -4,16 +4,13 @@
 import { u8aConcat } from '@polkadot/util';
 import { utils as utilsNobleSecp256k1 } from '@polkadot/x-noble-secp256k1';
 
-import { randomAsU8a } from './random/asU8a';
 import { cryptoWaitReady } from './crypto';
 import { hmacSha256AsU8a } from './hmac';
 
 // Set overrides on the secp256k1 utils
 //   - hmacShaSync - This needs to be set, unset by default
-//   - randomBytes - Use our implementation for full platform support
 utilsNobleSecp256k1.hmacSha256Sync = (key: Uint8Array, ...messages: Uint8Array[]) =>
   hmacSha256AsU8a(key, u8aConcat(...messages));
-utilsNobleSecp256k1.randomBytes = randomAsU8a;
 
 // start init process immediately
 cryptoWaitReady().catch((): void => {
