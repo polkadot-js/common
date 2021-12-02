ed20aa4a18d7f7f324d1b9760a2470cca1976bfb
Jaco <jacogr@gmail.com>
2021-12-02 15:19:35 +0200

    sign (tests failing)

--- packages/util-crypto/src/secp256k1/sign.spec.ts
+++ packages/util-crypto/src/secp256k1/sign.spec.ts
@@ -2,18 +2,30 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { hexToU8a } from '@polkadot/util';
+import { waitReady } from '@polkadot/wasm-crypto';
 
+import { performanceWasm } from '../../test/performance';
 import { secp256k1PairFromSeed, secp256k1Sign } from '.';
 
-describe('sign', (): void => {
-  it('generates a known signature', (): void => {
-    const msg = hexToU8a('0xa30b64ce1eedf409c8afb801d72c05234e64849ea538c15dd3c8cf4ffcf166c9');
-    const pair = secp256k1PairFromSeed(hexToU8a('0x4380de832af797688026ce24f85204d508243f201650c1a134929e5458b7fbae'));
+const pair = secp256k1PairFromSeed(hexToU8a('0x4380de832af797688026ce24f85204d508243f201650c1a134929e5458b7fbae'));
+const msg = hexToU8a('0xa30b64ce1eedf409c8afb801d72c05234e64849ea538c15dd3c8cf4ffcf166c9');
 
-    expect(
-      secp256k1Sign(msg, pair)
-    ).toBe(hexToU8a(
-      '0xdf92f73d9f060cefacf187b5414491cb992998ace017fa48839b5cda3e264ba8c4efa521361678d9b8582744d77aa4b8d886d7380b7808a683174afad9c4700300'
-    ));
+describe('sign', (): void => {
+  beforeEach(async (): Promise<void> => {
+    await waitReady();
   });
+
+  describe.each([false, true])('onlyJs=%p', (onlyJs): void => {
+    it('generates a known signature', (): void => {
+      expect(
+        secp256k1Sign(msg, pair, undefined, onlyJs)
+      ).toBe(hexToU8a(
+        '0xdf92f73d9f060cefacf187b5414491cb992998ace017fa48839b5cda3e264ba8c4efa521361678d9b8582744d77aa4b8d886d7380b7808a683174afad9c4700300'
+      ));
+    });
+  });
+
+  performanceWasm('secp256k1Sign', 200, (_, onlyJs) =>
+    secp256k1Sign(msg, pair, undefined, onlyJs)
+  );
 });
