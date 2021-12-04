3c2c285b30c1d9ca6c1aa1d1ef36d5b903baebb3
Jaco <jacogr@gmail.com>
2021-12-04 12:19:42 +0200

    Remove own version of hashes

--- packages/x-noble-hashes/src/index.ts
+++
@@ -1,5 +0,0 @@
-/*! noble-hashes - MIT License (c) 2021 Paul Miller (paulmillr.com) */
-// https://github.com/paulmillr/noble-hashes/pull/13
-throw new Error(
-  'noble-hashes have no entry-point. Please consult the README.md to learn how to use them'
-);
