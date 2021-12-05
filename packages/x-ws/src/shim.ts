7ab553bb8311e3c66f8bc8788b1982c215b44594
Jaco <jacogr@gmail.com>
2021-12-05 09:45:13 +0200

    Add shims for x-* (not used, convenience)

---
+++ packages/x-ws/src/shim.ts
@@ -0,0 +1,9 @@
+// Copyright 2017-2021 @polkadot/x-textencoder authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { xglobal } from '@polkadot/x-global';
+import { WebSocket } from '@polkadot/x-ws';
+
+if (typeof xglobal.WebSocket !== 'function') {
+  (xglobal as Record<string, unknown>).WebSocket = WebSocket;
+}
