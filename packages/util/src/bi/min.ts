1b57833697c2d020d30500625bdfce4be2310ddf
Jaco <jacogr@gmail.com>
2021-12-08 11:25:26 +0200

    Dedupes for min/max

--- packages/util/src/bi/min.ts
+++ packages/util/src/bi/min.ts
@@ -3,20 +3,40 @@
 
 import { assert } from '../assert';
 
-/**
- * @name nMin
- * @summary Finds and returns the lowest value in an array of bigint.
- */
-export function nMin (...items: bigint[]): bigint {
+function gt (a: bigint, b: bigint): boolean {
+  return a > b;
+}
+
+function lt (a: bigint, b: bigint): boolean {
+  return a < b;
+}
+
+function find (items: bigint[], cmp: (a: bigint, b: bigint) => boolean): bigint {
   assert(items.length >= 1, 'Must provide one or more bigint arguments');
 
   let result = items[0];
 
   for (let i = 1; i < items.length; i++) {
-    if (items[i] < result) {
+    if (cmp(items[i], result)) {
       result = items[i];
     }
   }
 
   return result;
+}
+
+/**
+ * @name nMax
+ * @summary Finds and returns the highest value in an array of bigint.
+ */
+export function nMax (...items: bigint[]): bigint {
+  return find(items, gt);
+}
+
+/**
+ * @name nMin
+ * @summary Finds and returns the lowest value in an array of bigint.
+ */
+export function nMin (...items: bigint[]): bigint {
+  return find(items, lt);
 }
