434bf8ee456d49af52bdb816d27c2fd5109bd620
Jared Norris <jarednorris@gmail.com>
2021-12-09 11:13:29 -0600

    Fixed sort order

--- packages/hw-ledger/src/defaults.ts
+++ packages/hw-ledger/src/defaults.ts
@@ -4,11 +4,10 @@
 import type Transport from '@ledgerhq/hw-transport';
 import type { SubstrateApp } from '@zondax/ledger-substrate';
 
-import { newSoraApp, newBifrostApp, newCentrifugeApp, newDockApp, newEdgewareApp, newEquilibriumApp, newGenshiroApp, newKusamaApp, newNodleApp, newPolkadotApp, newPolymeshApp, newStatemineApp } from '@zondax/ledger-substrate';
+import { newBifrostApp, newCentrifugeApp, newDockApp, newEdgewareApp, newEquilibriumApp, newGenshiroApp, newKusamaApp, newNodleApp, newPolkadotApp, newPolymeshApp, newSoraApp, newStatemineApp } from '@zondax/ledger-substrate';
 
 // These match up with the keys of the knownLedger object in the @polkadot/networks/defaults.ts
 export const ledgerApps: Record<string, (transport: Transport) => SubstrateApp> = {
-  sora: newSoraApp,
   bifrost: newBifrostApp,
   centrifuge: newCentrifugeApp,
   'dock-mainnet': newDockApp,
@@ -19,5 +18,6 @@ export const ledgerApps: Record<string, (transport: Transport) => SubstrateApp> 
   'nodle-chain': newNodleApp,
   polkadot: newPolkadotApp,
   polymesh: newPolymeshApp,
+  sora: newSoraApp,
   statemine: newStatemineApp
 };
