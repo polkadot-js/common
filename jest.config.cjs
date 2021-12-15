77357e553c8cf78eeb9d5c5888d5bbd6de3524f5
Jaco <jacogr@gmail.com>
2021-12-15 16:03:21 +0200

    Remove x-noble-* packages (all upstream)

--- jest.config.cjs
+++ jest.config.cjs
@@ -14,7 +14,6 @@ module.exports = Object.assign({}, config, {
     // eslint-disable-next-line sort-keys
     '@polkadot/util(.*)$': '<rootDir>/packages/util/src/$1',
     '@polkadot/x-(bigint|global)(.*)$': '<rootDir>/packages/x-$1/src/$2',
-    '@polkadot/x-(fetch|randomvalues|textdecoder|textencoder|ws)(.*)$': '<rootDir>/packages/x-$1/src/node',
-    '@polkadot/x-noble-(hashes|secp256k1)(.*)$': '<rootDir>/packages/x-noble-$1/src/$2'
+    '@polkadot/x-(fetch|randomvalues|textdecoder|textencoder|ws)(.*)$': '<rootDir>/packages/x-$1/src/node'
   }
 });
--- packages/x-noble-hashes/src/index.ts
+++
@@ -1,3 +0,0 @@
-throw new Error(
-  'noble-hashes have no entry-point. Please consult the README.md to learn how to use them'
-);
