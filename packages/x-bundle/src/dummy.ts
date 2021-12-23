df1f11c5a87eba16dadeaa44643b71bcca914003
Jaco <jacogr@gmail.com>
2021-12-23 08:55:16 +0200

    Fix build

---
+++ packages/x-bundle/src/dummy.ts
@@ -0,0 +1,8 @@
+// Copyright 2017-2021 @polkadot/x-bundle authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// This is a dummy, aka we don't check requires and then have
+// some issues with the fact that we have a util reference
+import { BN } from '@polkadot/util';
+
+export { BN };
