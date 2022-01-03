041c6ea0bc6352144ea406dea533c3e66ceb0f24
StefansArya <stefansarya1@gmail.com>
2022-01-04 03:59:41 +0700

    Use global `crypto` object for browser and `webcrypto` for Node.js

--- packages/x-bundle/src/crypto.ts
+++ packages/x-bundle/src/crypto.ts
@@ -1,4 +1,12 @@
 // Copyright 2017-2022 @polkadot/x-bundle authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export default {};
+import { xglobal } from '@polkadot/x-global';
+
+let crypto = xglobal.crypto;
+
+if (crypto === undefined && typeof require === 'function') {
+	crypto = require('crypto').webcrypto;
+}
+
+export default crypto;
