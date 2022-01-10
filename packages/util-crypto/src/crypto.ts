9356c52f2886b7f9589686bf77cbf5025e6f16d1
Jaco <jacogr@gmail.com>
2022-01-10 10:48:47 +0100

    Crypto init failure test (#1370)

--- packages/util-crypto/src/crypto.ts
+++ packages/util-crypto/src/crypto.ts
@@ -1,16 +1,17 @@
 // Copyright 2017-2022 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { assert } from '@polkadot/util';
 import { isReady, waitReady } from '@polkadot/wasm-crypto';
 
 export const cryptoIsReady = isReady;
 
 export function cryptoWaitReady (): Promise<boolean> {
   return waitReady()
-    .then((): boolean => true)
-    .catch((error): boolean => {
-      console.error('Unable to initialize @polkadot/util-crypto', error);
+    .then((): boolean => {
+      assert(isReady(), 'Unable to initialize @polkadot/util-crypto');
 
-      return false;
-    });
+      return true;
+    })
+    .catch(() => false);
 }
