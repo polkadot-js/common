a5cdf83ef36ad06acb64a6e65bea1f200d6abcbf
Jaco <jacogr@gmail.com>
2022-01-10 11:37:24 +0200

    Crypto init failure test

---
+++ packages/util-crypto/src/crypto.spec.ts
@@ -0,0 +1,16 @@
+// Copyright 2017-2022 @polkadot/util authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+describe('cryptoWaitReady', (): void => {
+  it('should return false when it cannot initialize', async (): Promise<void> => {
+    const old = global.WebAssembly;
+
+    global.WebAssembly = null as unknown as typeof WebAssembly;
+
+    const { cryptoWaitReady } = await import('./crypto');
+
+    expect(await cryptoWaitReady()).toBe(false);
+
+    global.WebAssembly = old;
+  });
+});
