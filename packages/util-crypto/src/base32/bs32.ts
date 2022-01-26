11d42ff785a822f9779f52feba883c61cd6b7d8d
Jaco <jacogr@gmail.com>
2022-01-26 07:33:29 +0200

    Swap to @scure/base (micro-base rename)

--- packages/util-crypto/src/base32/bs32.ts
+++ packages/util-crypto/src/base32/bs32.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2022 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { utils } from 'micro-base';
+import { utils } from '@scure/base';
 
 import { createDecode, createEncode, createIs, createValidate } from './helpers';
 
