e825e5b3b15395aabe65203546c9e106288c39bf
Jaco <jacogr@gmail.com>
2022-02-11 10:16:37 +0200

    Remove require

--- packages/networks/src/ss58registry.spec.ts
+++ packages/networks/src/ss58registry.spec.ts
@@ -5,14 +5,14 @@ import known from '@substrate/ss58-registry';
 import fs from 'fs';
 import path from 'path';
 
+import other from './test/ss58registry.test.json';
+
 describe('@substrate/ss58-registry', (): void => {
   it('has known values', (): void => {
     const json = JSON.stringify(known, null, 2);
-    const cmpPath = path.join(__dirname, './test/ss58registry.test.json');
 
     try {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      expect(JSON.parse(json)).toEqual(require(cmpPath));
+      expect(JSON.parse(json)).toEqual(other);
     } catch (error) {
       if (process.env.GITHUB_REPOSITORY) {
         console.error(json);
@@ -20,7 +20,7 @@ describe('@substrate/ss58-registry', (): void => {
         throw error;
       }
 
-      fs.writeFileSync(cmpPath, json, { flag: 'w' });
+      fs.writeFileSync(path.join(__dirname, './test/ss58registry.test.json'), json, { flag: 'w' });
     }
   });
 });
