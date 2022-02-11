73898ada4365b93ad5263fee3438477bf827740e
Jaco <jacogr@gmail.com>
2022-02-11 09:52:35 +0100

    ss58registry test (#1406)
    * ss58registry test
    
    * ss58 reg update
    
    * Remove require
    
    * json resolve
    
    * Include test output

---
+++ packages/networks/src/ss58registry.spec.ts
@@ -0,0 +1,26 @@
+// Copyright 2017-2022 @polkadot/networks authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import known from '@substrate/ss58-registry';
+import fs from 'fs';
+import path from 'path';
+
+import other from './test/ss58registry.test.json';
+
+describe('@substrate/ss58-registry', (): void => {
+  it('has known values', (): void => {
+    const json = JSON.stringify(known, null, 2);
+
+    try {
+      expect(JSON.parse(json)).toEqual(other);
+    } catch (error) {
+      if (process.env.GITHUB_REPOSITORY) {
+        console.error(json);
+
+        throw error;
+      }
+
+      fs.writeFileSync(path.join(__dirname, './test/ss58registry.test.json'), json, { flag: 'w' });
+    }
+  });
+});
