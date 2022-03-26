3328d735239796437e84d636bf4748a5d52cce8c
Jaco <jacogr@gmail.com>
2022-03-26 17:04:31 +0200

    Adjust imports

--- packages/util-crypto/src/secp256k1/signVerify.spec.ts
+++ packages/util-crypto/src/secp256k1/signVerify.spec.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2022 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import '../cryptoInit';
+import '../bundleInit';
 
 import { stringToU8a } from '@polkadot/util';
 
