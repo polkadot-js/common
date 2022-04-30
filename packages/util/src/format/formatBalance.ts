e24385213d8074733c13457d532851ef855f3726
Jaco <jacogr@gmail.com>
2022-04-30 07:25:32 +0200

    9.1.1 (#1473)

--- packages/util/src/format/formatBalance.ts
+++ packages/util/src/format/formatBalance.ts
@@ -21,10 +21,25 @@ interface SetDefaults {
 }
 
 interface Options {
+  /**
+   * @description The number of decimals
+   */
   decimals?: number;
+  /**
+   * @description Format the number with this specific unit
+   */
   forceUnit?: string;
+  /**
+   * @description Format with SI, i.e. m/M/etc.
+   */
   withSi?: boolean;
+  /**
+   * @description Format with full SI, i.e. mili/Mega/etc.
+   */
   withSiFull?: boolean;
+  /**
+   * @description Add the unit (useful in Balance formats)
+   */
   withUnit?: boolean | string;
 }
 
