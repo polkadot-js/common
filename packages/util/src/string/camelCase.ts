823fb51d18482fe263493778a1fcc832de90dc4e
Jaco <jacogr@gmail.com>
2022-05-27 08:15:56 +0200

    Use for loop in string{Came, Pascal}Case conversions (#1504)
    * Use for loop in string{Came, Pascal}Case conversions
    
    * Missing +-= (removed as test)

--- packages/util/src/string/camelCase.ts
+++ packages/util/src/string/camelCase.ts
@@ -7,50 +7,59 @@ import type { AnyString } from '../types';
 //
 // this is not as optimal as the original answer (we split into multiple),
 // however it does pass the tests (which the original doesn't) and it is still
-// a 10+x improvement over the original camelcase npm package (at running)
+// a major improvement over the original camelcase npm package (at running)
 //
 // original: 20.88 μs/op
-//     this:  2.86 μs/op
+//     this:  1.20 μs/op
 //
 // Caveat of this: only Ascii, but acceptable for the intended usecase
-function converter (fn: (w: string, i: number) => string): (value: AnyString) => string {
-  const format = (w: string, i: number) => fn(w[0], i) + w.slice(1);
+function converter (format: (w: string, i: number) => string): (value: AnyString) => string {
+  const formatRuns = (w: string) => w.slice(0, w.length - 1).toLowerCase() + w.slice(-1).toUpperCase();
 
-  return (value: AnyString): string =>
-    value
+  return (value: AnyString): string => {
+    const parts = value
       .toString()
       // replace all seperators (including consequtive) with spaces
       .replace(/[-_., ]+/g, ' ')
       // we don't want leading or trailing spaces
       .trim()
       // split into words
-      .split(' ')
+      .split(' ');
+    let result = '';
+
+    for (let i = 0; i < parts.length; i++) {
+      const w = parts[i];
+
       // apply the formatting
-      .map((w, i) =>
-        format(
-          w.toUpperCase() === w
-            // all full uppercase + letters are changed to lowercase
-            ? w.toLowerCase()
-            // all consecutive capitals + letters are changed to lowercase
-            // e.g. UUID64 -> uuid64, while preserving splits, eg. NFTOrder -> nftOrder
-            : w.replace(/^[A-Z0-9]{2,}[^a-z]/, (w) =>
-              w.slice(0, w.length - 1).toLowerCase() + w.slice(-1).toUpperCase()
-            ),
-          i
-        )
-      )
-      // combine into a single word
-      .join('');
+      result += format(
+        w.toUpperCase() === w
+          // all full uppercase + letters are changed to lowercase
+          ? w.toLowerCase()
+          // all consecutive capitals + letters are changed to lowercase
+          // e.g. UUID64 -> uuid64, while preserving splits, eg. NFTOrder -> nftOrder
+          : w.replace(/^[A-Z0-9]{2,}[^a-z]/, formatRuns),
+        i
+      );
+    }
+
+    return result;
+  };
 }
 
 /**
  * @name stringCamelCase
  * @summary Convert a dash/dot/underscore/space separated Ascii string/String to camelCase
  */
-export const stringCamelCase = converter((w, i) => i ? w.toUpperCase() : w.toLowerCase());
+export const stringCamelCase = converter((w, i) =>
+  // lowercase for first letter/first word, else uppercase first, rest unchanged
+  (i ? w[0].toUpperCase() : w[0].toLowerCase()) + w.slice(1)
+);
 
 /**
  * @name stringPascalCase
  * @summary Convert a dash/dot/underscore/space separated Ascii string/String to PascalCase
  */
-export const stringPascalCase = converter((w) => w.toUpperCase());
+export const stringPascalCase = converter((w) =>
+  // uppercase the first character, leave the rest unchanged
+  w[0].toUpperCase() + w.slice(1)
+);
