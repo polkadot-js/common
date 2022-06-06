6b7dffa9eaedb08ea4dc8bc6ed7acb909fa1971f
Jaco <jacogr@gmail.com>
2022-06-06 08:51:08 +0300

    Update packages/networks/src/defaults/genesis.ts

--- packages/networks/src/defaults/genesis.ts
+++ packages/networks/src/defaults/genesis.ts
@@ -21,7 +21,8 @@ export const knownGenesis: KnownGenesis = {
     '0x9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed'
   ],
   centrifuge: [
-    '0xb3db41421702df9a7fcac62b53ffeac85f7853cc4e689e0b93aeb3db18c09d82'
+    '0xb3db41421702df9a7fcac62b53ffeac85f7853cc4e689e0b93aeb3db18c09d82',
+    '0x67dddf2673b69e5f875f6f25277495834398eafd67f492e09f3f3345e003d1b5'
   ],
   composable: [
     '0xdaab8df776eb52ec604a5df5d388bb62a050a0aaec4556a64265b9d42755552d'
