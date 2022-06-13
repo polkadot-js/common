f38145a8db749cc9c3ce652b39537649ca0683f3
Jaco <jacogr@gmail.com>
2022-06-13 15:49:08 +0300

    Adjust

--- packages/util/src/bn/bn.ts
+++ packages/util/src/bn/bn.ts
@@ -1,15 +1,18 @@
 // Copyright 2017-2022 @polkadot/util authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import * as bn from 'bn.js';
+import { BN as BNjs } from 'bn.js';
 
-class BN extends bn.BN {
-  // wrapper class - this allows us to get the typings correct, ala
-  // the bn.js import is _slightly_ weird between the class/namespace
-  //
-  // In this case we want to ensure it works the same way as with
-  // esModuleInterop, i.e. the type `BN` is allowed and the `new BN(...)`
-  // works as expected in all cases (returning something of type BN)
+/**
+ * @name BN
+ * @description
+ * Wrapper class - this allows us to get the typings correct, ala
+ * the bn.js import is _slightly_ weird between the class/namespace
+
+ * In this case we want to ensure it works the same way as with
+ * esModuleInterop, i.e. the type `BN` is allowed and the `new BN(...)`
+ * works as expected in all cases (returning something of type BN)
+ */
+export class BN extends BNjs {
+  // since we have no additional interfaces, these 2 are equivalent
 }
-
-export { BN };
