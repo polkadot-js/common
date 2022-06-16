fff30601a0e89e8e9947e40be4fafc449164d5a5
Jaco <jacogr@gmail.com>
2022-06-16 10:29:23 +0200

    Unroll u8aToBn loops (#1535)
    * Unroll u8aToBn loops
    
    * Adjust with additional xor shortcuts
    
    * Adjust mult -> shift (where below u32)
    
    * assertUnreachable
    
    * Add u8aToNumber

--- packages/util/src/array/zip.ts
+++ packages/util/src/array/zip.ts
@@ -1,6 +1,10 @@
 // Copyright 2017-2022 @polkadot/util authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/**
+ * @name arrayZip
+ * @description Combines 2 distinct key/value arrays into a single [K, V] array
+ */
 export function arrayZip <K, V> (keys: K[], values: V[]): [K, V][] {
   const result = new Array<[K, V]>(keys.length);
 
--- packages/util/src/compact/fromU8a.ts
+++ packages/util/src/compact/fromU8a.ts
@@ -30,35 +30,38 @@ export function compactFromU8a (input: U8aLike): [number, BN] {
       return [1, new BN(u8a[0] >>> 2)];
 
     case 0b01:
-      return [2, new BN((u8a[0] + (u8a[1] * 0x1_00)) >>> 2)];
+      return [2, new BN((u8a[0] + (u8a[1] << 8)) >>> 2)];
 
     case 0b10:
-      return [4, new BN((u8a[0] + (u8a[1] * 0x1_00) + (u8a[2] * 0x1_00_00) + (u8a[3] * 0x1_00_00_00)) >>> 2)];
+      // for the 3rd byte, we don't << 24 - since JS converts all bitwise operators to
+      // 32-bit, in the case where the top-most bit is set this yields a negative value
+      return [4, new BN((u8a[0] + (u8a[1] << 8) + (u8a[2] << 16) + (u8a[3] * 0x1_00_00_00)) >>> 2)];
 
     // 0b11
-    default:
-      break;
-  }
+    default: {
+      // add 5 to shifted (4 for base length, 1 for this byte)
+      const offset = (u8a[0] >>> 2) + 5;
 
-  // add 5 to shifted (4 for base length, 1 for this byte)
-  const offset = (u8a[0] >>> 2) + 5;
+      // we unroll the loop
+      switch (offset) {
+        // there still could be 4 bytes data, similar to 0b10 above (with offsets)
+        case 5:
+          // for the 3rd byte, we don't << 24 - since JS converts all bitwise operators to
+          // 32-bit, in the case where the top-most bit is set this yields a negative value
+          return [5, new BN(u8a[1] + (u8a[2] << 8) + (u8a[3] << 16) + (u8a[4] * 0x1_00_00_00))];
 
-  // we unroll the loop
-  switch (offset) {
-    // there still could be 4 bytes data, similar to 0b10 above (with offsets)
-    case 5:
-      return [5, new BN(u8a[1] + (u8a[2] * 0x1_00) + (u8a[3] * 0x1_00_00) + (u8a[4] * 0x1_00_00_00))];
+        case 6:
+          return [6, new BN(u8a[1] + (u8a[2] << 8) + (u8a[3] << 16) + ((u8a[4] + (u8a[5] << 8)) * 0x1_00_00_00))];
 
-    case 6:
-      return [6, new BN(u8a[1] + (u8a[2] * 0x1_00) + (u8a[3] * 0x1_00_00) + (u8a[4] * 0x1_00_00_00) + (u8a[5] * 0x1_00_00_00_00))];
+        // 6 bytes data is the maximum, 48 bits (56 would overflow)
+        case 7:
+          return [7, new BN(u8a[1] + (u8a[2] << 8) + (u8a[3] << 16) + ((u8a[4] + (u8a[5] << 8) + (u8a[6] << 16)) * 0x1_00_00_00))];
 
-    // 6 bytes data is the maximum, 48 bits (56 would overflow)
-    case 7:
-      return [7, new BN(u8a[1] + (u8a[2] * 0x1_00) + (u8a[3] * 0x1_00_00) + (u8a[4] * 0x1_00_00_00) + (u8a[5] * 0x1_00_00_00_00) + (u8a[6] * 0x1_00_00_00_00_00))];
-
-    // for anything else, use the non-unrolled version
-    default:
-      return [offset, u8aToBn(u8a.subarray(1, offset))];
+        // for anything else, use the non-unrolled version
+        default:
+          return [offset, u8aToBn(u8a.subarray(1, offset))];
+      }
+    }
   }
 }
 
@@ -74,34 +77,33 @@ export function compactFromU8aLim (u8a: Uint8Array): [number, number] {
       return [1, u8a[0] >>> 2];
 
     case 0b01:
-      return [2, (u8a[0] + (u8a[1] * 0x1_00)) >>> 2];
+      return [2, (u8a[0] + (u8a[1] << 8)) >>> 2];
 
     case 0b10:
-      return [4, (u8a[0] + (u8a[1] * 0x1_00) + (u8a[2] * 0x1_00_00) + (u8a[3] * 0x1_00_00_00)) >>> 2];
+      // for the 3rd byte, we don't << 24 - since JS converts all bitwise operators to
+      // 32-bit, in the case where the top-most bit is set this yields a negative value
+      return [4, (u8a[0] + (u8a[1] << 8) + (u8a[2] << 16) + (u8a[3] * 0x1_00_00_00)) >>> 2];
 
     // 0b11
-    default:
-      break;
-  }
+    default: {
+      // add 5 to shifted (4 for base length, 1 for this byte)
+      // we unroll the loop
+      switch ((u8a[0] >>> 2) + 5) {
+        // there still could be 4 bytes data, similar to 0b10 above (with offsets)
+        case 5:
+          return [5, u8a[1] + (u8a[2] << 8) + (u8a[3] << 16) + (u8a[4] * 0x1_00_00_00)];
 
-  // add 5 to shifted (4 for base length, 1 for this byte)
-  const offset = (u8a[0] >>> 2) + 5;
+        case 6:
+          return [6, u8a[1] + (u8a[2] << 8) + (u8a[3] << 16) + ((u8a[4] + (u8a[5] << 8)) * 0x1_00_00_00)];
 
-  // we unroll the loop
-  switch (offset) {
-    // there still could be 4 bytes data, similar to 0b10 above (with offsets)
-    case 5:
-      return [5, u8a[1] + (u8a[2] * 0x1_00) + (u8a[3] * 0x1_00_00) + (u8a[4] * 0x1_00_00_00)];
+        // 6 bytes data is the maximum, 48 bits (56 would overflow)
+        case 7:
+          return [7, u8a[1] + (u8a[2] << 8) + (u8a[3] << 16) + ((u8a[4] + (u8a[5] << 8) + (u8a[6] << 16)) * 0x1_00_00_00)];
 
-    case 6:
-      return [6, u8a[1] + (u8a[2] * 0x1_00) + (u8a[3] * 0x1_00_00) + (u8a[4] * 0x1_00_00_00) + (u8a[5] * 0x1_00_00_00_00)];
-
-    // 6 bytes data is the maximum, 48 bits (56 would overflow)
-    case 7:
-      return [7, u8a[1] + (u8a[2] * 0x1_00) + (u8a[3] * 0x1_00_00) + (u8a[4] * 0x1_00_00_00) + (u8a[5] * 0x1_00_00_00_00) + (u8a[6] * 0x1_00_00_00_00_00)];
-
-    // for anything else, we are above the actual MAX_SAFE_INTEGER - bail out
-    default:
-      throw new Error('Compact input is > Number.MAX_SAFE_INTEGER');
+        // for anything else, we are above the actual MAX_SAFE_INTEGER - bail out
+        default:
+          throw new Error('Compact input is > Number.MAX_SAFE_INTEGER');
+      }
+    }
   }
 }
