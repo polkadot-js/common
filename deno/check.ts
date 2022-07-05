771f60a3511899c9d215f6ee18a0515b0b850b08
Jaco <jacogr@gmail.com>
2022-07-06 00:15:37 +0300

    Additional CI actions

---
+++ deno/check.ts
@@ -0,0 +1,18 @@
+// Copyright 2019-2022 @polkadot/util authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// auto-generated via deno/setup.ts, do not edit
+
+export * as hwLedger from 'https://deno.land/x/polkadot/hw-ledger/mod.ts';
+export * as hwLedgerTransports from 'https://deno.land/x/polkadot/hw-ledger-transports/mod.ts';
+export * as keyring from 'https://deno.land/x/polkadot/keyring/mod.ts';
+export * as networks from 'https://deno.land/x/polkadot/networks/mod.ts';
+export * as util from 'https://deno.land/x/polkadot/util/mod.ts';
+export * as utilCrypto from 'https://deno.land/x/polkadot/util-crypto/mod.ts';
+export * as xBigint from 'https://deno.land/x/polkadot/x-bigint/mod.ts';
+export * as xFetch from 'https://deno.land/x/polkadot/x-fetch/mod.ts';
+export * as xGlobal from 'https://deno.land/x/polkadot/x-global/mod.ts';
+export * as xRandomvalues from 'https://deno.land/x/polkadot/x-randomvalues/mod.ts';
+export * as xTextdecoder from 'https://deno.land/x/polkadot/x-textdecoder/mod.ts';
+export * as xTextencoder from 'https://deno.land/x/polkadot/x-textencoder/mod.ts';
+export * as xWs from 'https://deno.land/x/polkadot/x-ws/mod.ts';
