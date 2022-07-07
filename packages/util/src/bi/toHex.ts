286d9e1517c65076aa310bdb783831221c4a1d86
Jaco <jacogr@gmail.com>
2022-07-07 10:46:33 +0300

    Pass bitLength parameter

--- packages/util/src/bi/toHex.ts
+++ packages/util/src/bi/toHex.ts
@@ -14,10 +14,8 @@ const ZERO_STR = '0x00';
  * @name nToHex
  * @summary Creates a hex value from a bigint object.
  */
-export function nToHex <ExtToBn extends ToBn | ToBigInt> (value?: ExtToBn | BN | bigint | number | null, { isLe = false, isNegative = false }: NumberOptions = {}): HexString {
+export function nToHex <ExtToBn extends ToBn | ToBigInt> (value?: ExtToBn | BN | bigint | number | null, { bitLength, isLe = false, isNegative = false }: NumberOptions = {}): HexString {
   return !value
     ? ZERO_STR
-    : u8aToHex(
-      nToU8a(value, { isLe, isNegative })
-    );
+    : u8aToHex(nToU8a(value, { bitLength, isLe, isNegative }));
 }
