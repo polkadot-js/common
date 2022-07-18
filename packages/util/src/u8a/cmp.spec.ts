b7c6e0ad1795875a4ff65a15522d4147a4e38ed2
Jaco <jacogr@gmail.com>
2022-07-18 14:47:48 +0300

    Small performance adjustments for Uint8Array indexing

--- packages/util/src/u8a/cmp.spec.ts
+++ packages/util/src/u8a/cmp.spec.ts
@@ -1,7 +1,24 @@
 // Copyright 2017-2022 @polkadot/util authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+import { perf } from '../test/performance';
 import { u8aCmp } from '.';
+
+const ltest = new Uint8Array(32768);
+const stest = new Uint8Array(256);
+const ztest = new Uint8Array(64);
+
+for (let i = 0; i < ltest.length; i++) {
+  if (i < ztest.length) {
+    ztest[i] = i % 256;
+  }
+
+  if (i < stest.length) {
+    stest[i] = i % 256;
+  }
+
+  ltest[i] = i % 256;
+}
 
 describe('u8aCmp', (): void => {
   it('has the same result as localCompare (equal length)', (): void => {
@@ -36,4 +53,8 @@ describe('u8aCmp', (): void => {
       )
     ).toEqual(0);
   });
+
+  perf('u8aCmp (64 cmp)', 100_000, [[ztest, ztest]], u8aCmp);
+  perf('u8aCmp (256 cmp)', 50_000, [[stest, stest]], u8aCmp);
+  perf('u8aCmp (32k cmp)', 500, [[ltest, ltest]], u8aCmp);
 });
--- packages/util/src/u8a/empty.ts
+++ packages/util/src/u8a/empty.ts
@@ -8,12 +8,12 @@
  * Checks to see if the input `Uint8Array` has zero length or contains all 0 values.
  */
 export function u8aEmpty (value: Uint8Array): boolean {
-  const len = value.length;
+  const len = value.length | 0;
 
   // on smaller sizes, the byte-by-byte compare is faster than allocating
   // another object for DataView (on very large arrays the DataView is faster)
   for (let i = 0; i < len; i++) {
-    if (value[i]) {
+    if (value[i] | 0) {
       return false;
     }
   }
