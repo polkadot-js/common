9c05257046a8981bc28f13cb6e2f5670ebbf28d1
Jaco <jacogr@gmail.com>
2022-07-20 10:23:26 +0200

    Add ability to spread Map entries (optimization) (#1612)

--- packages/util/src/object/spread.spec.ts
+++ packages/util/src/object/spread.spec.ts
@@ -17,5 +17,17 @@ describe('objectSpread', (): void => {
     ).toEqual({ a: 2, b: 2, c: { d: 3, e: 4 } });
   });
 
-  perf('objectSpread', 500_000, [[]], () => objectSpread({}, { a: 1, b: 2, c: 3 }, { d: 4, e: 7, f: 6 }));
+  it('spreads a Map', (): void => {
+    const map = new Map<string, string>([['foo', '123']]);
+
+    expect(
+      objectSpread({ bar: '123' }, map)
+    ).toEqual({
+      bar: '123',
+      foo: '123'
+    });
+  });
+
+  perf('objectSpread', 350_000, [[{}, { a: 1, b: 2, c: 3 }, { d: 4, e: 7, f: 6 }]], objectSpread);
+  perf('objectSpread', 350_000, [[{}, new Map<string, number>([['a', 1], ['b', 2], ['c', 3]]), new Map<string, number>([['d', 4], ['e', 5], ['f', 6]])]], objectSpread);
 });
