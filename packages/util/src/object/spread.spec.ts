6d46f8b24e095c38bb9562eff77adf0315bdea66
Jaco <jacogr@gmail.com>
2022-07-21 07:33:15 +0200

    10.1.1 (#1614)

--- packages/util/src/object/spread.spec.ts
+++ packages/util/src/object/spread.spec.ts
@@ -5,10 +5,22 @@ import { perf } from '../test/performance';
 import { objectSpread } from '.';
 
 describe('objectSpread', (): void => {
+  it('spreads obj sources', (): void => {
+    expect(
+      objectSpread({}, { a: 1, b: 2, c: 3 }, { d: 4, e: 5, f: 6 })
+    ).toEqual({ a: 1, b: 2, c: 3, d: 4, e: 5, f: 6 });
+  });
+
+  it('spreads map sources', (): void => {
+    expect(
+      objectSpread({}, new Map<string, number>([['a', 1], ['b', 2], ['c', 3]]), new Map<string, number>([['d', 4], ['e', 5], ['f', 6]]))
+    ).toEqual({ a: 1, b: 2, c: 3, d: 4, e: 5, f: 6 });
+  });
+
   it('spreads an object from multiple sources', (): void => {
     expect(
-      objectSpread({ a: 1 }, { b: 2 }, { a: 2, c: { d: 3, e: 4 } })
-    ).toEqual({ a: 2, b: 2, c: { d: 3, e: 4 } });
+      objectSpread({ a: 1 }, { b: 2 }, { a: 2, c: { d: 3, e: 4 } }, new Map<string, unknown>([['f', 5], ['g', 6], ['h', { i: 7, k: 8 }]]))
+    ).toEqual({ a: 2, b: 2, c: { d: 3, e: 4 }, f: 5, g: 6, h: { i: 7, k: 8 } });
   });
 
   it('spreads an object from multiple sources (with null & undefined)', (): void => {
@@ -17,17 +29,6 @@ describe('objectSpread', (): void => {
     ).toEqual({ a: 2, b: 2, c: { d: 3, e: 4 } });
   });
 
-  it('spreads a Map', (): void => {
-    const map = new Map<string, string>([['foo', '123']]);
-
-    expect(
-      objectSpread({ bar: '123' }, map)
-    ).toEqual({
-      bar: '123',
-      foo: '123'
-    });
-  });
-
-  perf('objectSpread', 350_000, [[{}, { a: 1, b: 2, c: 3 }, { d: 4, e: 7, f: 6 }]], objectSpread);
-  perf('objectSpread', 350_000, [[{}, new Map<string, number>([['a', 1], ['b', 2], ['c', 3]]), new Map<string, number>([['d', 4], ['e', 5], ['f', 6]])]], objectSpread);
+  perf('objectSpread (obj)', 350_000, [[{}, { a: 1, b: 2, c: 3 }, { d: 4, e: 5, f: 6 }]], objectSpread);
+  perf('objectSpread (map)', 350_000, [[{}, new Map<string, number>([['a', 1], ['b', 2], ['c', 3]]), new Map<string, number>([['d', 4], ['e', 5], ['f', 6]])]], objectSpread);
 });
