57a0513d31b1abc73d49a68f492caf5f09016381
Jaco <jacogr@gmail.com>
2022-08-19 11:20:08 +0300

    Add new @zondax/ledger-substrate

--- packages/hw-ledger/src/defaults.spec.ts
+++ packages/hw-ledger/src/defaults.spec.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2022 @polkadot/hw-ledger authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { supportedApps } from '@zondax/ledger-substrate/dist/supported_apps';
+import { supportedApps } from '@zondax/ledger-substrate';
 
 import { ledgerApps } from './defaults';
 
