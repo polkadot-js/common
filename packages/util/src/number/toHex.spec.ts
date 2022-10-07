b78e89d9e7c395f279748d27115b7c54e2bc1cf0
Jaco <jacogr@gmail.com>
2022-10-07 06:51:17 +0300

    Correct numberToHex(0) conversion

--- packages/util/src/number/toHex.spec.ts
+++ packages/util/src/number/toHex.spec.ts
@@ -4,22 +4,22 @@
 import { numberToHex } from '.';
 
 describe('numberToHex', (): void => {
-  it('converts undefined to 0x', (): void => {
+  it('converts undefined to 0x00', (): void => {
     expect(
       numberToHex()
-    ).toEqual('0x');
+    ).toEqual('0x00');
   });
 
-  it('converts null to 0x', (): void => {
+  it('converts null to 0x00000000 (with bitLength)', (): void => {
     expect(
-      numberToHex(null)
-    ).toEqual('0x');
+      numberToHex(null, 32)
+    ).toEqual('0x00000000');
   });
 
   it('converts Nan to 0x', (): void => {
     expect(
       numberToHex(NaN)
-    ).toEqual('0x');
+    ).toEqual('0x00');
   });
 
   it('converts 0 to 0x00', (): void => {
