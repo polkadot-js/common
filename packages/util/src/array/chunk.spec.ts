69319b950bd10de2497cb17df6a53eed04f6e893
Jaco <jacogr@gmail.com>
2023-01-22 08:34:29 +0200

    Expand arrayChunk tests

--- packages/util/src/array/chunk.spec.ts
+++ packages/util/src/array/chunk.spec.ts
@@ -5,10 +5,22 @@ import { perf } from '../test/performance';
 import { arrayChunk, arrayRange } from '.';
 
 describe('arrayChunk', (): void => {
-  it('chunks arrays', (): void => {
+  it('chunks with exact', (): void => {
+    expect(
+      arrayChunk([1, 2, 3, 4, 5, 6, 7, 8], 8)
+    ).toEqual([[1, 2, 3, 4, 5, 6, 7, 8]]);
+  });
+
+  it('chunks with unequal', (): void => {
     expect(
       arrayChunk([1, 2, 3, 4, 5, 6, 7], 3)
     ).toEqual([[1, 2, 3], [4, 5, 6], [7]]);
+  });
+
+  it('chunks with non-empty results', (): void => {
+    expect(
+      arrayChunk([[1, 2], [3, 4], [5, 6], [7, 8]], 2)
+    ).toEqual([[[1, 2], [3, 4]], [[5, 6], [7, 8]]]);
   });
 
   perf('arrayChunk', 200_000, [[arrayRange(500), 50]], arrayChunk);
