e787f4f1d9d33c3c7ab858beeb9181a8dd448afb
Jaco <jacogr@gmail.com>
2023-02-27 14:59:51 +0200

    Swap x-ws to use ws

--- packages/x-ws/src/node.ts
+++ packages/x-ws/src/node.ts
@@ -1,10 +1,10 @@
 // Copyright 2017-2023 @polkadot/x-ws authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import ws from 'websocket';
+import ws from 'ws';
 
 import { extractGlobal } from '@polkadot/x-global';
 
 export { packageInfo } from './packageInfo';
 
-export const WebSocket = extractGlobal('WebSocket', ws.w3cwebsocket);
+export const WebSocket = extractGlobal('WebSocket', ws);
