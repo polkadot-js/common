ff0eeb9e61a9795ff10b53d36d193cd4d32b5294
Jaco <jacogr@gmail.com>
2023-03-14 07:50:49 +0200

    Remove encrypt/decrypt tests

--- packages/keyring/src/pair/index.spec.ts
+++ packages/keyring/src/pair/index.spec.ts
@@ -18,10 +18,6 @@ await cryptoWaitReady();
 
 describe('pair', (): void => {
   const SIGNATURE = new Uint8Array([80, 191, 198, 147, 225, 207, 75, 88, 126, 39, 129, 109, 191, 38, 72, 181, 75, 254, 81, 143, 244, 79, 237, 38, 236, 141, 28, 252, 134, 26, 169, 234, 79, 33, 153, 158, 151, 34, 175, 188, 235, 20, 35, 135, 83, 120, 139, 211, 233, 130, 1, 208, 201, 215, 73, 80, 56, 98, 185, 196, 11, 8, 193, 14]);
-  const ENCRYPTED = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 132, 59, 53, 85, 227, 40, 107, 48, 234, 198, 238, 157, 238, 224, 235, 179, 75, 153, 241, 142, 254]);
-
-  // the last byte is changed from 254 -> 253
-  const ENCRYPTED_CHANGED = new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 132, 59, 53, 85, 227, 40, 107, 48, 234, 198, 238, 157, 238, 224, 235, 179, 75, 153, 241, 142, 253]);
 
   it('has a publicKey', (): void => {
     expect(
@@ -96,93 +92,6 @@ describe('pair', (): void => {
         keyring.bob.publicKey
       )
     ).toBe(false);
-  });
-
-  it('allows encrypting', (): void => {
-    const message = new Uint8Array([0x61, 0x62, 0x63, 0x64, 0x65]);
-
-    expect(
-      keyring.alice.encryptMessage(
-        message,
-        keyring.bob.publicKey,
-        new Uint8Array(24)
-      )
-    ).toEqual(ENCRYPTED);
-  });
-
-  it('validates a correctly encrypted message', (): void => {
-    const message = new Uint8Array([0x61, 0x62, 0x63, 0x64, 0x65]);
-
-    expect(
-      keyring.bob.decryptMessage(
-        ENCRYPTED,
-        keyring.alice.publicKey
-      )
-    ).toEqual(message);
-  });
-
-  it('fails a correctly encrypted message (message changed)', (): void => {
-    expect(
-      keyring.bob.decryptMessage(
-        ENCRYPTED_CHANGED,
-        keyring.alice.publicKey
-      )
-    ).toEqual(null);
-  });
-
-  it('fails a correctly encrypted message (sender changed)', (): void => {
-    expect(
-      keyring.bob.decryptMessage(
-        ENCRYPTED,
-        keyring.charlie.publicKey
-      )
-    ).toEqual(null);
-  });
-
-  it('fails a correctly encrypted message (receiver changed)', (): void => {
-    expect(
-      keyring.charlie.decryptMessage(
-        ENCRYPTED,
-        keyring.alice.publicKey
-      )
-    ).toEqual(null);
-  });
-
-  it('validates encryption between ed25519 & sr25519 key pairs', (): void => {
-    const PASS = 'testing';
-    const encodedA = keyring.alice.encodePkcs8(PASS);
-    const encodedB = keyring.bob.encodePkcs8(PASS);
-
-    const pairA = createPair({ toSS58, type: 'sr25519' }, { publicKey: keyring.alice.publicKey });
-    const pairB = createPair({ toSS58, type: 'sr25519' }, { publicKey: keyring.bob.publicKey });
-
-    pairA.decodePkcs8(PASS, encodedA);
-    pairB.decodePkcs8(PASS, encodedB);
-    const message = new Uint8Array([0x61, 0x62, 0x63, 0x64, 0x65]);
-
-    // alice-sr25519 -> bob-ed25519
-    const encrypted1 = pairA.encryptMessage(message, keyring.bob.publicKey);
-
-    // alice-sr25519 -> bob-sr25519
-    const encrypted2 = pairA.encryptMessage(message, pairB.publicKey);
-
-    // alice-ed25519 -> bob-sr25519
-    const encrypted3 = keyring.alice.encryptMessage(message, pairB.publicKey);
-
-    // decrypt: Bob-ed25519 - use alice-ed25519 pubkey
-    expect(
-      keyring.bob.decryptMessage(encrypted3, keyring.alice.publicKey)
-    ).toEqual(message);
-
-    // decrypt: Bob-ed25519 - use alice-sr25519 pubkey
-    expect(
-      keyring.bob.decryptMessage(encrypted1, pairA.publicKey)
-    ).toEqual(message);
-
-    // decrypt: Bob-sr25519 - use alice-sr25519 pubkey
-    expect(
-      pairB.decryptMessage(encrypted2, pairA.publicKey)
-    ).toEqual(message);
   });
 
   it('allows setting/getting of meta', (): void => {
@@ -275,23 +184,6 @@ describe('pair', (): void => {
         version: '3'
       });
       expect(json.address).toEqual(u8aToHex(PUBLICDERIVED));
-    });
-
-    it('denies access to encryptMessage/decryptMessage API', (): void => {
-      const PASS = 'testing';
-      const encoded = keyring.alice.encodePkcs8(PASS);
-
-      const pair = createPair({ toSS58, type: 'ethereum' }, { publicKey: keyring.alice.publicKey });
-
-      pair.decodePkcs8(PASS, encoded);
-
-      expect(
-        () => pair.encryptMessage(
-          new Uint8Array(4), // null message
-          keyring.bob.publicKey,
-          new Uint8Array(24)
-        )
-      ).toThrow('Secp256k1 not supported yet');
     });
   });
 });
