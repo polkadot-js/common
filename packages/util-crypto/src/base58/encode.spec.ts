6e541ee03c799d3c0a93f9c81366eede583b620c
Jaco <jacogr@gmail.com>
2023-03-17 07:09:41 +0200

    Bump dev deps

--- packages/util-crypto/src/base58/encode.spec.ts
+++ packages/util-crypto/src/base58/encode.spec.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2023 @polkadot/util-crypto authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-/// <reference types="@polkadot/dev-test/node.d.ts" />
+/// <reference types="@polkadot/dev-test/globals.d.ts" />
 
 import { base32Decode } from '../base32/index.js';
 import { base58Encode } from './index.js';
--- packages/x-textdecoder/src/node.spec.ts
+++ packages/x-textdecoder/src/node.spec.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2023 @polkadot/x-textencoder authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-/// <reference types="@polkadot/dev-test/node.d.ts" />
+/// <reference types="@polkadot/dev-test/globals.d.ts" />
 
 import { xglobal } from '@polkadot/x-global';
 
