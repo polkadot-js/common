a6ed339af111370ee302582ae943bc9ee032e2cc
Jaco <jacogr@gmail.com>
2023-03-28 14:02:56 +0300

    Adjust for rollup bundles

--- packages/x-crypto/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright 2017-2023 @polkadot/x-crypto authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import { xglobal } from '@polkadot/x-global';
-
-export { packageInfo } from './packageInfo.js';
-
-export const crypto = xglobal.crypto;
--- packages/x-randomvalues/src/index.ts
+++ packages/x-randomvalues/src/index.ts
@@ -1,13 +1,10 @@
 // Copyright 2017-2023 @polkadot/x-randomvalues authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { crypto as xcrypto } from '@polkadot/x-crypto';
+import { crypto } from '@polkadot/x-crypto';
 
 export { packageInfo } from './packageInfo.js';
 
 export function getRandomValues <T extends Uint8Array> (arr: T): T {
-  // We use xcrypto here - this prevents packagers such as rollup
-  // confusing this with the "normal" Node.js import and stubbing it
-  // (and also aligns with eg. x-fetch, where x-global is used)
-  return xcrypto.getRandomValues(arr);
+  return crypto.getRandomValues(arr);
 }
