7c023c8ee905080504b9f453403fc1f67a12cca3
Jaco <jacogr@gmail.com>
2023-03-28 16:29:40 +0300

    export * for x-ws

--- packages/x-ws/src/mod.ts
+++ packages/x-ws/src/mod.ts
@@ -1,8 +1,4 @@
 // Copyright 2017-2023 @polkadot/x-ws authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export { packageInfo } from './packageInfo.js';
-
-const _WebSocket = WebSocket;
-
-export { _WebSocket as WebSocket };
+export * from './browser.js';
