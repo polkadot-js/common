21dc1b88f968bf8a8a3bb51b8c0f29df02f4a65e
Jaco <jacogr@gmail.com>
2023-05-18 11:34:28 +0300

    Adjust tsconfig usage (w/ latest dev)

--- packages/x-bundle/src/buffer-es6.d.ts
+++
@@ -1,4 +0,0 @@
-// Copyright 2017-2023 @polkadot/x-bundle authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-declare module 'buffer-es6';
--- packages/x-bundle/src/buffer.ts
+++ packages/x-bundle/src/buffer.ts
@@ -1,6 +1,7 @@
 // Copyright 2017-2023 @polkadot/x-bundle authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+// @ts-expect-error Only used in rollup bundling, we are not adding a declaration
 import { Buffer } from 'buffer-es6';
 
 export default Buffer;
