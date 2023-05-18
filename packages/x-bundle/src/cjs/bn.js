0bf7bc416a626b244766f19ad775cbe5ec574da0
Jaco <jacogr@gmail.com>
2023-05-18 08:29:07 +0300

    Bump dev deps (#1830)

--- packages/x-bundle/src/cjs/bn.js
+++ packages/x-bundle/src/cjs/bn.js
@@ -1,6 +1,7 @@
 // Copyright 2017-2023 @polkadot/x-bundle authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+// @ts-expect-error In dev mode we don't have export maps...
 const { BN } = require('@polkadot/util');
 
 module.exports = BN;
