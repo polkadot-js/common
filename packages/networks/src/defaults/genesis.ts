54e7ff3b1ddead62dabf0259f0bf452be08b73c3
Kami <46442452+Nick-1979@users.noreply.github.com>
2023-07-16 20:26:57 +0330

    Update genesis.ts

--- packages/networks/src/defaults/genesis.ts
+++ packages/networks/src/defaults/genesis.ts
@@ -155,7 +155,7 @@ export const knownGenesis: KnownGenesis = {
   westend: [
     '0xe143f23803ac50e8f6f8e62695d1ce9e4e1d68aa36c1cd2cfd15340213f3423e'
   ],
-  westmint = [
+  westmint: [
     '0x67f9723393ef76214df0118c34bbbd3dbebc8ed46a10973a8c969d48fe7598c9'
   ],
   xxnetwork: [
