66765fab020a3120c4c7d870d22a26112be13ea4
Jaco <jacogr@gmail.com>
2023-08-16 11:05:03 +0300

    Adjust linting

--- packages/util/src/lazy.ts
+++ packages/util/src/lazy.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2023 @polkadot/types authors & contributors
+// Copyright 2017-2023 @polkadot/util authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
 type AnyFn = (...args: unknown[]) => unknown;
