54b65b071c03506a7c7eb19ea39d3efd5f28cf56
Jaco <jacogr@gmail.com>
2023-08-22 09:52:37 +0300

    toBn test array

--- packages/util/src/u8a/toBn.spec.ts
+++ packages/util/src/u8a/toBn.spec.ts
@@ -3,9 +3,45 @@
 
 /// <reference types="@polkadot/dev-test/globals.d.ts" />
 
-// import { BN } from '../bn/index.js';
 import { perf } from '../test/index.js';
 import { u8aToBn } from './index.js';
+
+// TODO Share this with toNumber & toBigInt (expended there)
+// eslint-disable-next-line jest/no-export
+export const TESTS: [isLe: boolean, isNegative: boolean, numarr: number[], strval: string][] = [
+  // LE, positive numbers
+  [true, false, [0x12], '18'],
+  [true, false, [0x12, 0x34], '13330'],
+  [true, false, [0x12, 0x34, 0x56], '5649426'],
+  [true, false, [0x12, 0x34, 0x56, 0x78], '2018915346'],
+  [true, false, [0x12, 0x34, 0x56, 0x78, 0x9a], '663443878930'],
+  [true, false, [0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc], '207371629900818'],
+  [true, false, [0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78], '159954953172672629770948536149615195154'],
+  // LE, positivive numbers (w/ signed flag)
+  [true, true, [12], '12'],
+  [true, true, [210, 4], '1234'],
+  [true, true, [64, 226, 1], '123456'],
+  [true, true, [21, 205, 91, 7], '123456789'],
+  [true, true, [203, 36, 104, 12, 8], '34567890123'],
+  [true, true, [255, 159, 114, 78, 24, 9], '9999999999999'],
+  // LE, negative numbers
+  [true, true, [244], '-12'],
+  [true, true, [46, 251], '-1234'],
+  [true, true, [192, 29, 254], '-123456'],
+  [true, true, [235, 50, 164, 248], '-123456789'],
+  [true, true, [65, 86, 129, 173, 254], '-5678999999'],
+  [true, true, [1, 96, 141, 177, 231, 246], '-9999999999999'],
+  [true, true, [1, 0, 156, 88, 76, 73, 31, 242], '-999999999999999999'],
+  // BE
+  [false, false, [0x12], '18'],
+  [false, false, [0x12, 0x34], '4660'],
+  [false, false, [0x12, 0x34, 0x56], '1193046'],
+  [false, false, [0x12, 0x34, 0x56, 0x78], '305419896'],
+  [false, true, [0xf2, 0x34, 0x56, 0x78], '-231451016'],
+  [false, false, [0x12, 0x34, 0x56, 0x78, 0x9a], '78187493530'],
+  [false, false, [0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc], '20015998343868'],
+  [false, false, [0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78], '24197857161011715162171839636988778104']
+];
 
 // test-cases are the same as in u8aToBigInt
 describe('u8aToBn', (): void => {
@@ -17,283 +53,38 @@ describe('u8aToBn', (): void => {
     ).toBe('3412');
   });
 
+  describe('conversion tests', (): void => {
+    for (let i = 0, count = TESTS.length; i < count; i++) {
+      const [isLe, isNegative, numarr, strval] = TESTS[i];
+
+      it(`${i}: creates ${strval} (bitLength=${numarr.length * 8}, isLe=${isLe}, isNegative=${isNegative})`, (): void => {
+        expect(
+          u8aToBn(
+            new Uint8Array(numarr),
+            { isLe, isNegative }
+          ).toString()
+        ).toBe(strval);
+      });
+    }
+  });
+
   describe('LE', (): void => {
-    describe('unsigned', (): void => {
-      it('converts values (u8)', (): void => {
+    for (let i = 1; i < 32; i++) {
+      const tu8a = [0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78];
+      const tstr = tu8a.map((n) => n.toString(16));
+
+      it(`converts values with length ${i}`, (): void => {
         expect(
           u8aToBn(
-            new Uint8Array([0x12]),
+            new Uint8Array(tu8a.slice(0, i)),
             { isLe: true }
           ).toString(16)
-        ).toBe('12');
+        ).toBe(tstr.slice(0, i).reverse().join(''));
       });
-
-      it('converts values (u16)', (): void => {
-        expect(
-          u8aToBn(
-            new Uint8Array([0x12, 0x34]),
-            { isLe: true }
-          ).toString(16)
-        ).toBe('3412');
-      });
-
-      it('converts values (u24)', (): void => {
-        expect(
-          u8aToBn(
-            new Uint8Array([0x12, 0x34, 0x56]),
-            { isLe: true }
-          ).toString(16)
-        ).toBe('563412');
-      });
-
-      it('converts values (u32)', (): void => {
-        expect(
-          u8aToBn(
-            new Uint8Array([0x12, 0x34, 0x56, 0x78]),
-            { isLe: true }
-          ).toString(16)
-        ).toBe('78563412');
-      });
-
-      it('converts values (u40)', (): void => {
-        expect(
-          u8aToBn(
-            new Uint8Array([0x12, 0x34, 0x56, 0x78, 0x9a]),
-            { isLe: true }
-          ).toString(16)
-        ).toBe('9a78563412');
-      });
-
-      it('converts values (u48)', (): void => {
-        expect(
-          u8aToBn(
-            new Uint8Array([0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc]),
-            { isLe: true }
-          ).toString(16)
-        ).toBe('bc9a78563412');
-      });
-
-      it('converts values (u128)', (): void => {
-        expect(
-          u8aToBn(
-            new Uint8Array([0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78]),
-            { isLe: true }
-          ).toString(16)
-        ).toBe('78563412785634127856341278563412');
-      });
-
-      for (let i = 1; i < 32; i++) {
-        const tu8a = [0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78];
-        const tstr = tu8a.map((n) => n.toString(16));
-
-        it(`converts values with length ${i}`, (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array(tu8a.slice(0, i)),
-              { isLe: true }
-            ).toString(16)
-          ).toBe(tstr.slice(0, i).reverse().join(''));
-        });
-      }
-    });
-
-    describe('signed', (): void => {
-      describe('positive values', (): void => {
-        it('converts positive numbers (i8)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([12]),
-              { isNegative: true }
-            ).toNumber()
-          ).toBe(12);
-        });
-
-        it('converts positive numbers (i16)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([210, 4]),
-              { isNegative: true }
-            ).toNumber()
-          ).toBe(1234);
-        });
-
-        it('converts positive numbers (i24)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([64, 226, 1]),
-              { isNegative: true }
-            ).toNumber()
-          ).toBe(123456);
-        });
-
-        it('converts positive numbers (i32)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([21, 205, 91, 7]),
-              { isNegative: true }
-            ).toNumber()
-          ).toBe(123456789);
-        });
-
-        it('converts positive numbers (i40)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([203, 36, 104, 12, 8]),
-              { isNegative: true }
-            ).toNumber()
-          ).toBe(34567890123);
-        });
-
-        it('converts positive numbers (i48)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([255, 159, 114, 78, 24, 9]),
-              { isNegative: true }
-            ).toNumber()
-          ).toBe(9999999999999);
-        });
-      });
-
-      describe('negative values', (): void => {
-        it('converts values (i8)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([244]),
-              { isLe: true, isNegative: true }
-            ).toString()
-          ).toBe('-12');
-        });
-
-        it('converts values (i16)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([46, 251]),
-              { isLe: true, isNegative: true }
-            ).toString()
-          ).toBe('-1234');
-        });
-
-        it('converts values (i24)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([192, 29, 254]),
-              { isLe: true, isNegative: true }
-            ).toString()
-          ).toBe('-123456');
-        });
-
-        it('converts values (i32)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([235, 50, 164, 248]),
-              { isLe: true, isNegative: true }
-            ).toString()
-          ).toBe('-123456789');
-        });
-
-        it('converts values (i40)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([65, 86, 129, 173, 254]),
-              { isLe: true, isNegative: true }
-            ).toString()
-          ).toBe('-5678999999');
-        });
-
-        it('converts values (i48)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([1, 96, 141, 177, 231, 246]),
-              { isLe: true, isNegative: true }
-            ).toString()
-          ).toBe('-9999999999999');
-        });
-
-        it('converts values (i64)', (): void => {
-          expect(
-            u8aToBn(
-              new Uint8Array([1, 0, 156, 88, 76, 73, 31, 242]),
-              { isLe: true, isNegative: true }
-            ).toString()
-          ).toBe('-999999999999999999');
-        });
-      });
-    });
+    }
   });
 
   describe('BE', (): void => {
-    it('converts values (u8)', (): void => {
-      expect(
-        u8aToBn(
-          new Uint8Array([0x12]),
-          { isLe: false }
-        ).toString(16)
-      ).toBe('12');
-    });
-
-    it('converts values (u16)', (): void => {
-      expect(
-        u8aToBn(
-          new Uint8Array([0x12, 0x34]),
-          { isLe: false }
-        ).toString(16)
-      ).toBe('1234');
-    });
-
-    it('converts values (u24)', (): void => {
-      expect(
-        u8aToBn(
-          new Uint8Array([0x12, 0x34, 0x56]),
-          { isLe: false }
-        ).toString(16)
-      ).toBe('123456');
-    });
-
-    it('converts values (u32)', (): void => {
-      expect(
-        u8aToBn(
-          new Uint8Array([0x12, 0x34, 0x56, 0x78]),
-          { isLe: false }
-        ).toString(16)
-      ).toBe('12345678');
-    });
-
-    it('converts values (i32)', (): void => {
-      expect(
-        u8aToBn(
-          new Uint8Array([0xf2, 0x34, 0x56, 0x78]),
-          { isLe: false, isNegative: true }
-        ).toString(16)
-      ).toBe('-dcba988');
-    });
-
-    it('converts values (u40)', (): void => {
-      expect(
-        u8aToBn(
-          new Uint8Array([0x12, 0x34, 0x56, 0x78, 0x9a]),
-          { isLe: false }
-        ).toString(16)
-      ).toBe('123456789a');
-    });
-
-    it('converts values (u48)', (): void => {
-      expect(
-        u8aToBn(
-          new Uint8Array([0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc]),
-          { isLe: false }
-        ).toString(16)
-      ).toBe('123456789abc');
-    });
-
-    it('converts values (u128)', (): void => {
-      expect(
-        u8aToBn(
-          new Uint8Array([0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78]),
-          { isLe: false }
-        ).toString(16)
-      ).toBe('12345678123456781234567812345678');
-    });
-
     for (let i = 1; i < 32; i++) {
       const tu8a = [0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78, 0x12, 0x34, 0x56, 0x78];
       const tstr = tu8a.map((n) => n.toString(16));
