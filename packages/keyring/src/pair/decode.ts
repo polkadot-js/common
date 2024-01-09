bdd2e55f571086d5e717aa7131ea050a8c8e6de7
Jaco <jacogr@gmail.com>
2024-01-09 09:15:45 +0200

    Comments around pair generation 1-3

--- packages/keyring/src/pair/decode.ts
+++ packages/keyring/src/pair/decode.ts
@@ -2,46 +2,51 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { EncryptedJsonEncoding } from '@polkadot/util-crypto/types';
-import type { PairInfo } from './types.js';
 
 import { u8aEq } from '@polkadot/util';
 import { jsonDecryptData } from '@polkadot/util-crypto';
 
-import { PKCS8_DIVIDER, PKCS8_HEADER, PUB_LENGTH, SEC_LENGTH, SEED_LENGTH } from './defaults.js';
+import { PAIR_DIV, PAIR_HDR, PUB_LENGTH, SEC_LENGTH, SEED_LENGTH } from './defaults.js';
 
-const SEED_OFFSET = PKCS8_HEADER.length;
+const SEED_OFFSET = PAIR_HDR.length;
 
-type DecodeResult = PairInfo & {
-  secretKey: Uint8Array;
-};
-
-export function decodePair (passphrase?: string, encrypted?: Uint8Array | null, _encType?: EncryptedJsonEncoding | EncryptedJsonEncoding[]): DecodeResult {
+/**
+ * Decode a pair, taking into account the generation-specific formats and headers
+ *
+ * For divisor/headers, don't rely on the magic being static. These will
+ * change between generations, aka with the long-awaited 4th generation
+ * of the format. The external decode interface is the only way to use and decode these.
+ **/
+export function decodePair (passphrase?: string, encrypted?: Uint8Array | null, _encType?: EncryptedJsonEncoding | EncryptedJsonEncoding[]): { publicKey: Uint8Array; secretKey: Uint8Array } {
   const encType = Array.isArray(_encType) || _encType === undefined
     ? _encType
     : [_encType];
   const decrypted = jsonDecryptData(encrypted, passphrase, encType);
-  const header = decrypted.subarray(0, PKCS8_HEADER.length);
+  const header = decrypted.subarray(0, PAIR_HDR.length);
 
-  if (!u8aEq(header, PKCS8_HEADER)) {
-    throw new Error('Invalid Pkcs8 header found in body');
+  // check the start header (generations 1-3)
+  if (!u8aEq(header, PAIR_HDR)) {
+    throw new Error('Invalid encoding header found in body');
   }
 
+  // setup for generation 3 format
   let secretKey = decrypted.subarray(SEED_OFFSET, SEED_OFFSET + SEC_LENGTH);
   let divOffset = SEED_OFFSET + SEC_LENGTH;
-  let divider = decrypted.subarray(divOffset, divOffset + PKCS8_DIVIDER.length);
+  let divider = decrypted.subarray(divOffset, divOffset + PAIR_DIV.length);
 
-  // old-style, we have the seed here
-  if (!u8aEq(divider, PKCS8_DIVIDER)) {
+  // old-style (generation 1 & 2), we have the seed here
+  if (!u8aEq(divider, PAIR_DIV)) {
     divOffset = SEED_OFFSET + SEED_LENGTH;
     secretKey = decrypted.subarray(SEED_OFFSET, divOffset);
-    divider = decrypted.subarray(divOffset, divOffset + PKCS8_DIVIDER.length);
+    divider = decrypted.subarray(divOffset, divOffset + PAIR_DIV.length);
 
-    if (!u8aEq(divider, PKCS8_DIVIDER)) {
-      throw new Error('Invalid Pkcs8 divider found in body');
+    // check the divisior at this point (already checked for generation 3)
+    if (!u8aEq(divider, PAIR_DIV)) {
+      throw new Error('Invalid encoding divider found in body');
     }
   }
 
-  const pubOffset = divOffset + PKCS8_DIVIDER.length;
+  const pubOffset = divOffset + PAIR_DIV.length;
   const publicKey = decrypted.subarray(pubOffset, pubOffset + PUB_LENGTH);
 
   return {
--- packages/keyring/src/pair/encode.ts
+++ packages/keyring/src/pair/encode.ts
@@ -6,19 +6,19 @@ import type { PairInfo } from './types.js';
 import { u8aConcat } from '@polkadot/util';
 import { naclEncrypt, scryptEncode, scryptToU8a } from '@polkadot/util-crypto';
 
-import { PKCS8_DIVIDER, PKCS8_HEADER } from './defaults.js';
+import { PAIR_DIV, PAIR_HDR } from './defaults.js';
 
+/**
+ * Encode a pair with the latest generation format (generation 3)
+ *
+ * This is decodable with decodePair (which also deals with previous generations)
+ **/
 export function encodePair ({ publicKey, secretKey }: PairInfo, passphrase?: string): Uint8Array {
   if (!secretKey) {
     throw new Error('Expected a valid secretKey to be passed to encode');
   }
 
-  const encoded = u8aConcat(
-    PKCS8_HEADER,
-    secretKey,
-    PKCS8_DIVIDER,
-    publicKey
-  );
+  const encoded = u8aConcat(PAIR_HDR, secretKey, PAIR_DIV, publicKey);
 
   if (!passphrase) {
     return encoded;
