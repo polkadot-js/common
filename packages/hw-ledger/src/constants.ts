eb41ab5bed4b18c669daf2824b956609a65637a8
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2024-07-20 13:47:07 -0700

    Revert slip44 change (#1931)

--- packages/hw-ledger/src/constants.ts
+++ packages/hw-ledger/src/constants.ts
@@ -8,5 +8,3 @@ export const LEDGER_DEFAULT_CHANGE = 0x80000000;
 export const LEDGER_DEFAULT_INDEX = 0x80000000;
 
 export const LEDGER_SUCCESS_CODE = 0x9000;
-
-export const POLKADOT_SLIP = 0x00000162;
