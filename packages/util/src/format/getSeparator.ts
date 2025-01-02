5d03be10848640bfc769171c6112b386535ffdd2
Tarik Gul <47201679+TarikGul@users.noreply.github.com>
2025-01-02 07:37:40 -0800

    Fix toLocaleString for v22.12 (#1965)
    * Fix toLocaleString for v22.12
    
    * fix ci
    
    * Update tests, and cleanup solution
    
    * remove ar locale

--- packages/util/src/format/getSeparator.ts
+++ packages/util/src/format/getSeparator.ts
@@ -9,7 +9,7 @@
 export function getSeparator (locale?: string):
 { thousand: string, decimal: string } {
   return {
-    decimal: (0.1).toLocaleString(locale).substring(1, 2),
-    thousand: (1000).toLocaleString(locale).substring(1, 2)
+    decimal: (0.1).toLocaleString(locale, { useGrouping: false }).charAt(1),
+    thousand: (1000).toLocaleString(locale, { useGrouping: true }).replace(/\d/g, '').charAt(0)
   };
 }
