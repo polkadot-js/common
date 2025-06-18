2e47222eb572a8853fad406c83a801e875d5f1be
Arjun Porwal <porwalarjun95@gmail.com>
2025-06-18 09:30:03 +0530

    feat: update scrypt parameter validation to use ALLOWED_PARAMS

--- packages/util-crypto/src/scrypt/defaults.ts
+++ packages/util-crypto/src/scrypt/defaults.ts
@@ -3,8 +3,17 @@
 
 import type { ScryptParams } from './types.js';
 
+export const ALLOWED_PARAMS: ScryptParams[] = [
+  { N: 1 << 13, r: 8, p: 10 },
+  { N: 1 << 14, r: 8, p: 5 },
+  { N: 1 << 15, r: 8, p: 3 },
+  { N: 1 << 15, r: 8, p: 1 },
+  { N: 1 << 16, r: 8, p: 2 },
+  { N: 1 << 17, r: 8, p: 1 }
+];
+
 export const DEFAULT_PARAMS: ScryptParams = {
-  N: 1 << 15,
+  N: 1 << 17,
   p: 1,
   r: 8
 };
--- packages/util-crypto/src/scrypt/fromU8a.ts
+++ packages/util-crypto/src/scrypt/fromU8a.ts
@@ -6,7 +6,7 @@ import type { ScryptParams } from './types.js';
 import { u8aToBn } from '@polkadot/util';
 
 import { BN_LE_OPTS } from '../bn.js';
-import { DEFAULT_PARAMS } from './defaults.js';
+import { ALLOWED_PARAMS } from './defaults.js';
 
 interface Result {
   params: ScryptParams,
@@ -19,12 +19,15 @@ export function scryptFromU8a (data: Uint8Array): Result {
   const p = u8aToBn(data.subarray(32 + 4, 32 + 8), BN_LE_OPTS).toNumber();
   const r = u8aToBn(data.subarray(32 + 8, 32 + 12), BN_LE_OPTS).toNumber();
 
-  // FIXME At this moment we assume these to be fixed params, this is not a great idea
-  // since we lose flexibility and updates for greater security. However we need some
-  // protection against carefully-crafted params that can eat up CPU since these are user
-  // inputs. So we need to get very clever here, but atm we only allow the defaults
-  // and if no match, bail out
-  if (N !== DEFAULT_PARAMS.N || p !== DEFAULT_PARAMS.p || r !== DEFAULT_PARAMS.r) {
+  if (N > (1 << 20) || p > 4 || r > 16) {
+    throw new Error('Scrypt parameters exceed safe limits');
+  }
+
+  const isAllowed = ALLOWED_PARAMS.some((preset) =>
+    preset.N === N && preset.p === p && preset.r === r
+  );
+
+  if (!isAllowed) {
     throw new Error('Invalid injected scrypt params found');
   }
 
